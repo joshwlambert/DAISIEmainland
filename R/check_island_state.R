f494ed358e5d0facf28136c07d11caeb7588ff72
joshwlambert <j.w.l.lambert@rug.nl>
2021-02-09 20:36:00 +0000

    bug fix in check_island_state

--- R/check_island_state.R
+++ R/check_island_state.R
@@ -11,7 +11,7 @@ check_island_state <- function(timeval,
     immig_spec <- island_spec[which(island_spec[, 4] == "I"), 1]
     mainland_ex_time <- mainland[which(mainland[, 1] %in% immig_spec), 9]
     for (ex_time in mainland_ex_time) {
-      if (timeval > ex_time && totaltime < ex_time) {
+      if (timeval > ex_time && totaltime > ex_time) {
         island_spec[which(island_spec[, 1] == immig_spec), 4] <- "A"
         island_spec[which(island_spec[, 1] == immig_spec), 7] <-
           "mainland_extinction"
