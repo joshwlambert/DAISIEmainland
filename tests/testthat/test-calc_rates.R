0a89de87b0fba49dc4986d0e9120d73c9567ebd9
joshwlambert <j.w.l.lambert@rug.nl>
2021-02-10 20:12:49 +0000

    added calc_rates test

---
+++ tests/testthat/test-calc_rates.R
@@ -0,0 +1,19 @@
+context("calc_rates")
+
+test_that("calc_rates produces correct output", {
+  rates <- calc_rates(
+    timeval = 0.5,
+    totaltime = 1,
+    gam = 1,
+    laa = 1,
+    lac = 1,
+    mu = 1,
+    k = 10,
+    num_spec = 1,
+    num_immigrants = 1,
+    mainland_n = 10)
+  expect_equal(rates$immig_rate, 9)
+  expect_equal(rates$ext_rate, 1)
+  expect_equal(rates$ana_rate1, 1)
+  expect_equal(rates$clado_rate, 0.9)
+})
