10c05b5fb49dc40c6c2d619751e89e77d4445fc6
joshwlambert <j.w.l.lambert@rug.nl>
2021-02-21 21:53:12 +0100

    added names to expand.grid in generate_param_space

--- scripts/generate_param_space.R
+++ scripts/generate_param_space.R
@@ -1,23 +1,12 @@
-param_space <- expand.grid(c(5),
-                           c(100, 500, 1000),
-                           c(0.5, 1),
-                           c(0.5, 1),
-                           c(0.5, 1),
-                           c(0.01),
-                           c(10, Inf),
-                           c(0, 0.1, 0.3, 0.5),
-                           c(0.7, 0.8, 0.9, 1.0),
-                           c(1000))
-
-colnames(param_space) <- c("time",
-                           "m",
-                           "island_clado",
-                           "island_ex",
-                           "island_k",
-                           "island_immig",
-                           "island_ana",
-                           "mainland_ex",
-                           "mainland_sample_prob",
-                           "replicates")
+param_space <- expand.grid(time = c(5),
+                           m = c(100, 500, 1000),
+                           island_clado = c(0.5, 1),
+                           island_ex = c(0.5, 1),
+                           island_k = c(0.5, 1),
+                           island_immig = c(0.01),
+                           island_ana = c(10, Inf),
+                           mainland_ex = c(0, 0.1, 0.3, 0.5),
+                           mainland_sample_prob = c(0.7, 0.8, 0.9, 1.0),
+                           replicates = c(1000))
 
 write.csv(x = param_space, file = "data/param_space.csv", row.names = FALSE)
