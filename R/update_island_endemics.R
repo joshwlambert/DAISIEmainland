790f0151944fc817f740d61447288f62a297f9ac
joshwlambert <j.w.l.lambert@rug.nl>
2021-02-21 21:56:00 +0100

    updated update_island_endemics

--- R/update_island_endemics.R
+++ R/update_island_endemics.R
@@ -12,10 +12,10 @@ update_island_endemics <- function(
   if (any(island_spec[, 4] == "I")) {
     immig_spec <- island_spec[which(island_spec[, 4] == "I"), 1]
     mainland_ex_time <- mainland[which(mainland[, 1] %in% immig_spec), 9]
-    for (ex_time in mainland_ex_time) {
-      if (timeval > ex_time && totaltime > ex_time) {
-        island_spec[which(island_spec[, 1] == immig_spec), 4] <- "A"
-        island_spec[which(island_spec[, 1] == immig_spec), 7] <-
+    for (i in seq_along(mainland_ex_time)) {
+      if (timeval > mainland_ex_time[i] && totaltime > mainland_ex_time[i]) {
+        island_spec[which(island_spec[, 1] == immig_spec[i]), 4] <- "A"
+        island_spec[which(island_spec[, 1] == immig_spec[i]), 7] <-
           "mainland_extinction"
       }
     }
