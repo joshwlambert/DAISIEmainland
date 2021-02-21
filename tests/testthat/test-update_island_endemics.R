46de205492e1287a5114a90c8ccce3a08da268b5
joshwlambert <j.w.l.lambert@rug.nl>
2021-02-21 21:57:00 +0100

    used create_test_mainland in update_island_endemics tests

--- tests/testthat/test-update_island_endemics.R
+++ tests/testthat/test-update_island_endemics.R
@@ -1,38 +1,38 @@
 test_that("update_island_endemics produces correct output with empty island", {
-  # RJCB: I would love a 'create_test_mainland_1' function here!
-  mainland <- rbind(
-    c("1", "1", "0", "E", "A", NA, NA, "0", "0.292906805531114"),
-    c("15", "1", "0", "E", "AA", "0.292906805531114", NA, "0.292906805531114",
-      "0.541999222479509"),
-    c("16", "1", "0", "C", "AB", "0.292906805531114", NA, "0.292906805531114",
-      "1"))
+
+  mainland <- create_test_mainland(mainland_scenario = 1)
 
   island_state <- update_island_endemics(
     timeval = 0.05858136,
     totaltime = 1,
     island_spec = NULL,
-    mainland = mainland)
+    mainland = mainland[[1]])
   expect_null(island_state)
 })
 
 test_that("update_island_endemics produces correct output with non-empty island", {
-  # RJCB: I would love a 'create_test_mainland_2' function here!
-  mainland <- rbind(
-    c("1", "1", "0", "E", "A", NA, NA, "0", "0.292906805531114"),
-    c("15", "1", "0", "E", "AA", "0.292906805531114", NA, "0.292906805531114",
-      "0.541999222479509"),
-    c("16", "1", "0", "C", "AB", "0.292906805531114", NA, "0.292906805531114",
-      "1"))
+
+  mainland <- create_test_mainland(mainland_scenario = 2)
 
   island_state <- update_island_endemics(
     totaltime = 1,
     timeval = 0.6,
-    island_spec = rbind(c("15", "15", "0.292906805531114", "I", NA, NA, NA)),
-    mainland = mainland)
+    island_spec = data.frame(
+      spec_id = 15,
+      main_anc_id = 15,
+      col_t = 0.292906805531114,
+      spec_type = "I",
+      branch_code = NA,
+      branch_t = NA,
+      ana_origin = "mainland_extinction"),
+    mainland = mainland[[1]])
 
-  # I enjoy this test. I wish I would understand what 'check_island_state'
-  # does, so I could agree with this results by reading the input
   expect_equal(island_state,
-               rbind(c("15", "15", "0.292906805531114", "A", NA, NA,
-                       "mainland_extinction")))
+               data.frame(spec_id = c(15),
+                          main_anc_id = c(15),
+                          col_t = c(0.292906805531114),
+                          spec_type = c("A"),
+                          branch_code = c(NA),
+                          branch_t = c(NA),
+                          ana_origin = c("mainland_extinction")))
 })
