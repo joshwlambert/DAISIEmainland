80911c2af02e02d683a282f10354227b79ee6b62
joshwlambert <j.w.l.lambert@rug.nl>
2021-02-22 17:25:59 +0100

    updated sample_mainland

--- R/sample_mainland.R
+++ R/sample_mainland.R
@@ -11,13 +11,13 @@ sample_mainland <- function(
   if (mainland_sample_prob == 1) {
     return(mainland_clade)
   } else {
-    extant_spec <- which(mainland_clade[, 4] != "E")
+    extant_spec <- which(mainland_clade[, "spec_type"] != "E")
     sampled_spec <- stats::rbinom(n = length(extant_spec),
                                   size = 1,
                                   prob = mainland_sample_prob)
     extant_not_sampled <- extant_spec[which(sampled_spec == 0)]
-    mainland_clade[extant_not_sampled, 4] <- "NS"
-    mainland_clade[extant_not_sampled, 9] <- total_time - 1e-5
+    mainland_clade[extant_not_sampled, "spec_type"] <- "NS"
+    mainland_clade[extant_not_sampled, "spec_ex_t"] <- total_time - 1e-5
   }
   return(mainland_clade)
 }
