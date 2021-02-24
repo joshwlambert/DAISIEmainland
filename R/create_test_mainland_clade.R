2bdd14ba3e0f726d56ba28c531b889ebc4b909c1
joshwlambert <j.w.l.lambert@rug.nl>
2021-02-24 14:16:35 +0100

    added more mainland scenarios to create_test_mainland_clade and tests

--- R/create_test_mainland_clade.R
+++ R/create_test_mainland_clade.R
@@ -51,5 +51,39 @@ create_test_mainland_clade <- function(mainland_scenario) {
       spec_origin_t = c(0, 0.248564384071771, 0.248564384071771),
       spec_ex_t = c(0.248564384071771, 0.502788450052761, 0.502788450052761))
   }
+
+  if (mainland_scenario == 5) {
+
+    mainland_clade <- data.frame(
+      spec_id = c(1, 2, 3),
+      main_anc_id = c(1, 1, 1),
+      spec_type = c("E", "E", "C"),
+      branch_code = c("A", "AA", "AB"),
+      branch_t = c(NA, 0.5, 0.5),
+      spec_origin_t = c(0, 0.5, 0.5),
+      spec_ex_t = c(0.5, 0.75, 1.0))
+  }
+
+  if (mainland_scenario == 6) {
+    mainland_clade <- data.frame(
+      spec_id = c(1, 2, 3, 4, 5, 6, 7),
+      main_anc_id = c(1, 1, 1, 1, 1, 1, 1),
+      spec_type = c("E", "E", "E", "E", "C", "C", "C"),
+      branch_code = c("A", "AA", "AB", "AAA", "AAB", "ABA", "ABB"),
+      branch_t = c(NA, 0.5, 0.5, 0.6, 0.6, 0.7, 0.7),
+      spec_origin_t = c(0, 0.5, 0.5, 0.6, 0.6, 0.7, 0.7),
+      spec_ex_t = c(0.5, 0.6, 0.7, 0.8, 1.0, 1.0, 1.0))
+  }
+
+  if (mainland_scenario == 7) {
+    mainland_clade <- data.frame(
+      spec_id = c(1, 2, 3, 4, 5, 6, 7),
+      main_anc_id = c(1, 1, 1, 1, 1, 1, 1),
+      spec_type = c("E", "E", "E", "E", "E", "C", "C"),
+      branch_code = c("A", "AA", "AB", "AAA", "AAB", "ABA", "ABB"),
+      branch_t = c(NA, 0.5, 0.5, 0.6, 0.6, 0.7, 0.7),
+      spec_origin_t = c(0, 0.5, 0.5, 0.6, 0.6, 0.7, 0.7),
+      spec_ex_t = c(0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.0))
+  }
   return(mainland_clade)
 }
--- tests/testthat/test-create_test_mainland_clade.R
+++ tests/testthat/test-create_test_mainland_clade.R
@@ -25,3 +25,24 @@ test_that("create_test_mainland_clade produces correct output for scenario 4", {
   expect_true(ncol(mainland_clade) == 7)
   expect_true(nrow(mainland_clade) == 3)
 })
+
+test_that("create_test_mainland_clade produces correct output for scenario 5", {
+  mainland_clade <- create_test_mainland_clade(mainland_scenario = 5)
+  expect_true(is.data.frame(mainland_clade))
+  expect_true(ncol(mainland_clade) == 7)
+  expect_true(nrow(mainland_clade) == 3)
+})
+
+test_that("create_test_mainland_clade produces correct output for scenario 6", {
+  mainland_clade <- create_test_mainland_clade(mainland_scenario = 6)
+  expect_true(is.data.frame(mainland_clade))
+  expect_true(ncol(mainland_clade) == 7)
+  expect_true(nrow(mainland_clade) == 7)
+})
+
+test_that("create_test_mainland_clade produces correct output for scenario 7", {
+  mainland_clade <- create_test_mainland_clade(mainland_scenario = 7)
+  expect_true(is.data.frame(mainland_clade))
+  expect_true(ncol(mainland_clade) == 7)
+  expect_true(nrow(mainland_clade) == 7)
+})
