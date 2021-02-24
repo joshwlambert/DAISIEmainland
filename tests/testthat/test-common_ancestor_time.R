1d534117ea6047a1f700f65dee4f8196e6dabfb0
joshwlambert <j.w.l.lambert@rug.nl>
2021-02-24 14:15:51 +0100

    added tests for common_ancestor_time

--- tests/testthat/test-common_ancestor_time.R
+++ tests/testthat/test-common_ancestor_time.R
@@ -1,5 +1,35 @@
-test_that("common_ancestor_time runs silent and produces correct output", {
-  mainland_clade <- create_test_mainland_clade(mainland_scenario = 4)
+test_that("common_ancestor_time produces correct output for mainland_clade 5", {
+  mainland_clade <- create_test_mainland_clade(mainland_scenario = 5)
 
+  branching_t <- common_ancestor_time(
+    total_time = 1.0,
+    mainland_spec = 2,
+    mainland_clade = mainland_clade)
+
+  expect_equal(branching_t, 0.5)
 
 })
+
+test_that("common_ancestor_time produces correct output for mainland_clade 6", {
+  mainland_clade <- create_test_mainland_clade(mainland_scenario = 6)
+
+  branching_t <- common_ancestor_time(
+    total_time = 1.0,
+    mainland_spec = 4,
+    mainland_clade = mainland_clade)
+
+  expect_equal(branching_t, 0.4)
+})
+
+
+test_that("common_ancestor_time produces correct output for mainland_clade 7", {
+  mainland_clade <- create_test_mainland_clade(mainland_scenario = 7)
+
+  branching_t <- common_ancestor_time(
+    total_time = 1.0,
+    mainland_spec = 4,
+    mainland_clade = mainland_clade)
+
+  expect_equal(branching_t, 0.5)
+})
+
