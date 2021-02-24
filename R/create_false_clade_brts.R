84fb3784341e2ace360297713239ecfdb4af8693
joshwlambert <j.w.l.lambert@rug.nl>
2021-02-24 19:24:25 +0100

    added create_false_clade_brts function and test

---
+++ R/create_false_clade_brts.R
@@ -0,0 +1,24 @@
+#' Creates a vector of branching times when empirical data is a clade but
+#' the ideal data is multiple colonisations
+#'
+#' @inheritParams default_params_doc
+#'
+#' @return numeric vector
+#' @keywords internal
+create_false_clade_brts <- function(total_time,
+                                    anc_branch_t_bp,
+                                    subset_island) {
+  if (all(is.na(subset_island[, "branch_t_bp"]))) {
+    false_brts <- subset_island[, "col_t_bp"]
+    false_brts <- false_brts[-length(false_brts)]
+  } else {
+    false_brts <- subset_island[, "col_t_bp"]
+    false_brts <- false_brts[-length(false_brts)]
+    false_brts <- sort(
+      false_brts,
+      subset_island[, "branch_t_bp"],
+      decreasing = TRUE)
+  }
+  false_clade_brts <- c(total_time, anc_branch_t_bp, false_brts)
+  return(false_clade_brts)
+}
---
+++ tests/testthat/test-create_false_clade_brts.R
@@ -0,0 +1,19 @@
+test_that("create_false_clade_brts produces correct output", {
+  subset_island <- data.frame(
+    spec_id = c(119, 121, 125),
+    main_anc_id = c(118, 118, 118),
+    col_t_bp = c(0.884864028849, 0.623865697499, 0.406541763485),
+    spec_type = c("A", "A", "A"),
+    branch_code = c(NA, NA, NA),
+    branch_t_bp = c(NA, NA, NA),
+    ana_origin = c("Clado_extinct", "Clado_extinct", "Clado_extinct")
+  )
+
+  false_clade_brts <- create_false_clade_brts(
+    total_time = 1,
+    anc_branch_t_bp = 0.950145003459,
+    subset_island = subset_island)
+
+  expect_equal(false_clade_brts,
+               c(1.00000000000, 0.950145003459, 0.884864028849, 0.623865697499))
+})
