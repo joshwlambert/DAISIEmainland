f21fa553c9fdb904236f4fb52967a3563320e0f5
joshwlambert <j.w.l.lambert@rug.nl>
2021-03-02 14:06:12 +0100

    changed function in style test

--- tests/testthat/test-style.R
+++ tests/testthat/test-style.R
@@ -1,6 +1,4 @@
 test_that("follow Tidyverse style", {
-  skip("Run locally for now")
-  # Tip: use https://github.com/jimhester/lintr#github-actions
-  # when this is fixed
-  lintr::lint_package()
+  skip("WIP")
+  lintr::expect_lint_free()
 })
