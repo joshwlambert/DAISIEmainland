c65117e2441345d7a2064368aa08abae9afb0769
joshwlambert <j.w.l.lambert@rug.nl>
2021-03-03 20:10:10 +0100

    added another clado_event test

--- tests/testthat/test-clado_event.R
+++ tests/testthat/test-clado_event.R
@@ -1,4 +1,4 @@
-test_that("multiplication works", {
+test_that("clado_event produces correct output on non-cladogenesis species", {
   set.seed(1)
   timeval <- 0.6
   island_spec <- data.frame(spec_id = 1,
@@ -24,4 +24,32 @@ test_that("multiplication works", {
                                      ana_origin = c(NA, NA))
   expect_equal(updated_state$island_spec, expected_island_spec)
   expect_equal(updated_state$max_spec_id, 3)
+})
+
+test_that("clado_event produces correct output on cladogenesis species", {
+  set.seed(1)
+  timeval <- 0.9
+  island_spec <- data.frame(spec_id = c(2, 3),
+                            main_anc_id = c(1, 1),
+                            col_t = c(0.5, 0.5),
+                            spec_type = c("C", "C"),
+                            branch_code = c("A", "B"),
+                            branch_t = c(0.5, 0.7),
+                            ana_origin = c(NA, NA))
+  max_spec_id <- 3
+  updated_state <- clado_event(
+    timeval = timeval,
+    island_spec = island_spec,
+    max_spec_id = max_spec_id)
+
+  expect_true(is.list(updated_state))
+  expected_island_spec <- data.frame(spec_id = c(4, 3, 5),
+                                     main_anc_id = c(1, 1, 1),
+                                     col_t = c(0.5, 0.5, 0.5),
+                                     spec_type = c("C", "C", "C"),
+                                     branch_code = c("AA", "B", "AB"),
+                                     branch_t = c(0.5, 0.7, 0.9),
+                                     ana_origin = c(NA, NA, NA))
+  expect_equal(updated_state$island_spec, expected_island_spec)
+  expect_equal(updated_state$max_spec_id, 5)
 })
