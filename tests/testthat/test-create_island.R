66e4494b6cc62b93376d643dcd1dc0722762569b
Neves-P <p.m.santos.neves@rug.nl>
2021-03-12 17:08:33 +0100

    Use create_test_island_spec()

--- tests/testthat/test-create_island.R
+++ tests/testthat/test-create_island.R
@@ -28,13 +28,7 @@ test_that("mainland ancestor is extant; empirical = ideal", {
   total_time <- 3
 
   mainland_clade <- create_test_mainland_clade(mainland_scenario = 1)
-  island_spec <- data.frame(spec_id = 1,
-                            main_anc_id = 1,
-                            col_t = 2.413543,
-                            spec_type = "I",
-                            branch_code = "I",
-                            branch_t = NA,
-                            ana_origin = NA)
+  island_spec <- create_test_island_spec(1)
   mainland_sample_prob <- 1
   island <- create_island(
     total_time = total_time,
@@ -53,13 +47,7 @@ test_that("mainland ancestor is extinct; multiple colonists same species;
   total_time <- 3
 
   mainland_clade <- create_test_mainland_clade(mainland_scenario = 4)
-  island_spec <- data.frame(spec_id = c(149, 151, 150, 152),
-                            main_anc_id = c(37, 37, 147, 147),
-                            col_t = c(0.07551, 0.1226, 0.4587, 0.3716),
-                            spec_type = rep("A", 4),
-                            branch_code = rep(NA, 4),
-                            branch_t = rep(NA, 4),
-                            ana_origin = rep("immig_parent", 4))
+  island_spec <- create_test_island_spec(island_scenario = 2)
   mainland_sample_prob <- 1
 
   island <- create_island(
@@ -99,21 +87,15 @@ test_that("mainland ancestor is extinct; multiple colonists same species;
     island$ideal_island[[2]]$branching_times[1],
     island$ideal_island[[2]]$branching_times[2]
   )
-
 })
+
 test_that("mainland ancestor is extinct; multiple colonists same species;
           clado event", {
   set.seed(1)
   total_time <- 3
 
   mainland_clade <- create_test_mainland_clade(mainland_scenario = 4)
-  island_spec <- data.frame(spec_id = c(149, 151, 148, 152),
-                            main_anc_id = c(148, 148, 148, 148),
-                            col_t = c(0.2565, 0.2565, 0.4432, 0.2565),
-                            spec_type = c("C", "C", "I", "C"),
-                            branch_code = c("A", "BA", NA, "BB"),
-                            branch_t = c(0.2565, 0.2622, NA, 1.8829),
-                            ana_origin = rep(NA, 4))
+  island_spec <- create_test_island_spec(island_scenario = 3)
   mainland_sample_prob <- 1
 
   island <- create_island(
@@ -162,13 +144,7 @@ test_that("mainland ancestor is extinct; only one colonists same species;
   total_time <- 3
 
   mainland_clade <- create_test_mainland_clade(mainland_scenario = 4)
-  island_spec <- data.frame(spec_id = c(37, 149, 150),
-                            main_anc_id = c(37, 147, 147),
-                            col_t = c(0.1154, 0.4123, 0.4123),
-                            spec_type = c("I", "C", "C"),
-                            branch_code = c(NA, "A", "B"),
-                            branch_t = c(NA, 0.4123, 1.2302),
-                            ana_origin = rep(NA, 3))
+  island_spec <- create_test_island_spec(island_scenario = 4)
   mainland_sample_prob <- 1
 
   # sim_island(
@@ -199,8 +175,8 @@ test_that("mainland ancestor is extinct; only one colonists same species;
   # Currently 6, should be 2? Current output is that of "no branching event
   # between immigration and island age with extant descendants, and clade) FAILS
   testthat::expect_identical(island$empirical_island[[2]]$stac, 2)
-  testthat::expect_identical(island$ideal_island[[1]]$stac, 3)
-  testthat::expect_identical(island$ideal_island[[2]]$stac, 3)
+  testthat::expect_identical(island$ideal_island[[1]]$stac, 3) # should be 2?
+  testthat::expect_identical(island$ideal_island[[2]]$stac, 3) # should be 2?
   testthat::expect_equal(
     island$empirical_island[[1]]$branching_times[1],
     island$empirical_island[[1]]$branching_times[2],
