631800f006e5ce385ad5c53b968148603153cc23
joshwlambert <j.w.l.lambert@rug.nl>
2021-03-13 15:32:38 +0100

    added bash scripts

---
+++ bash/run_analysis.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --time=9-23:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=analysis
+#SBATCH --output=/data/p287218/DAISIEmainland/analysis.log
+#SBATCH --array=1-768
+#SBATCH --mem=1GB
+
+mkdir -p logs
+mkdir -p results
+ml R
+Rscript -e "remotes::install_github('joshwlambert/DAISIEmainland')"
+Rscript /data/p287218/DAISIEmainland/scripts/run_analysis ${SLURM_ARRAY_TASK_ID}
+
+
+
