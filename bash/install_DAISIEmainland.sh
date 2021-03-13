8ad889f4e42e4f4489bf0c28b76bbd26fb9943d9
joshwlambert <j.w.l.lambert@rug.nl>
2021-03-13 16:56:06 +0100

    moved install to separate bash script

---
+++ bash/install_DAISIEmainland.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --time=9-23:00:00
+#SBATCH --partition=gelifes
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=install_DAISIEmainland
+#SBATCH --output=/data/p287218/DAISIEmainland/install_DAISIEmainland.log
+#SBATCH --mem=1GB
+
+mkdir -p logs
+mkdir -p results
+ml R
+Rscript -e "remotes::install_github('joshwlambert/DAISIEmainland@main')"
--- bash/run_analysis_test.sh
+++ bash/run_analysis_test.sh
@@ -8,8 +8,4 @@
 #SBATCH --array=1-10
 #SBATCH --mem=1GB
 
-mkdir -p logs
-mkdir -p results
-ml R
-Rscript -e "remotes::install_github('joshwlambert/DAISIEmainland@main')"
 Rscript /data/p287218/DAISIEmainland/scripts/run_analysis_test.R ${SLURM_ARRAY_TASK_ID}
