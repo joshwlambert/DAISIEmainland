36510777c7778702c2b199ed4eb01d7822a9ade5
joshwlambert <j.w.l.lambert@rug.nl>
2021-03-13 20:14:06 +0100

    updated bash and R scripts and fixed bug in vignette

--- scripts/analysis/run_analysis.R
+++ scripts/analysis/run_analysis.R
@@ -1,11 +1,10 @@
 args <- commandArgs(TRUE)
-print(args)
+
 args <- as.numeric(args)
-print(args)
+
 data <- read.csv(file = "data/param_space.csv")
-print(nrow(data))
-replicates <- 10
-island <- DAISIEmainland::sim_island_with_mainland(
+
+island <- sim_island_with_mainland(
   total_time = data$total_time[args],
   m = data$m[args],
   island_pars = c(data$island_clado[args],
@@ -15,13 +14,13 @@ island <- DAISIEmainland::sim_island_with_mainland(
                   data$island_ana[args]),
   mainland_ex = data$mainland_ex[args],
   mainland_sample_prob = data$mainland_sample_prob[args],
-  replicates = replicates,
+  replicates = data$replicates[args],
   verbose = TRUE)
 
-ideal_ml <- vector("list", replicates)
-empirical_ml <- vector("list", replicates)
+ideal_ml <- vector("list", data$replicates[args])
+empirical_ml <- vector("list", data$replicates[args])
 
-for (i in seq_len(replicates)) {
+for (i in seq_len(data$replicates[args])) {
   ideal_ml[[i]] <- DAISIE::DAISIE_ML_CS(
     datalist = island$ideal_islands[[i]],
     initparsopt = c(data$island_clado[args],
@@ -36,7 +35,7 @@ for (i in seq_len(replicates)) {
     jitter = 1e-5)
 
   empirical_ml[[i]] <- DAISIE::DAISIE_ML_CS(
-    datalist = island$reality_islands[[i]],
+    datalist = island$empirical_islands[[i]],
     initparsopt = c(data$island_clado[args],
                     data$island_ex[args],
                     data$island_k[args],
@@ -56,11 +55,11 @@ output <- list(
 
 output_name <- paste0("param_set_", args)
 
-output_folder <- file.path(
-  getwd(),
-  "results")
+output_folder <- file.path(getwd(), "results")
 
 output_file_path <- file.path(output_folder, output_name)
 
-save(output, file = output_file_path)
+saveRDS(object = output, file = output_file_path)
+
+message("Finished")
 
