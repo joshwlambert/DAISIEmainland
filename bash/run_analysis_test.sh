162d7d41a064b50212b9e92df8f8fff874e493cd
joshwlambert <j.w.l.lambert@rug.nl>
2021-03-13 15:54:35 +0100

    adding test scripts

--- bash/run_analysis_test.sh
+++ bash/run_analysis_test.sh
@@ -3,13 +3,12 @@
 #SBATCH --partition=gelifes
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --job-name=analysis
-#SBATCH --output=/data/p287218/DAISIEmainland/analysis.log
-#SBATCH --array=1-768
+#SBATCH --job-name=analysis_test
+#SBATCH --output=/data/p287218/DAISIEmainland/analysis_test.log
 #SBATCH --mem=1GB
 
 mkdir -p logs
 mkdir -p results
 ml R
 Rscript -e "remotes::install_github('joshwlambert/DAISIEmainland')"
-Rscript /data/p287218/DAISIEmainland/scripts/run_analysis ${SLURM_ARRAY_TASK_ID}
+Rscript /data/p287218/DAISIEmainland/scripts/run_analysis_test.R $1
