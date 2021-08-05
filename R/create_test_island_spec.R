c9c2377eafd3317df74adc9fdb53a7dbfbadad7a
joshwlambert <j.w.l.lambert@rug.nl>
2021-08-05 13:32:12 +0200

    added island scenarios to create_test_island_spec

--- R/create_test_island_spec.R
+++ R/create_test_island_spec.R
@@ -6,7 +6,8 @@
 #' @keywords internal
 create_test_island_spec <- function(island_scenario) {
 
-  if (island_scenario == 1) {
+  if (island_scenario == 0) {
+    # Empty island
     island_spec <- data.frame(
       spec_id = numeric(),
       main_anc_id = numeric(),
@@ -17,29 +18,551 @@ create_test_island_spec <- function(island_scenario) {
       ana_origin = character())
   }
 
-  if (island_scenario == 2) {
-
+  if (island_scenario == 1) {
+    # Single lineage non-endemic (early col)
     island_spec <- data.frame(
       spec_id = 1,
       main_anc_id = 1,
-      col_t = 0.6,
+      col_t = 0.2,
       spec_type = "I",
       branch_code = NA,
       branch_t = NA,
       ana_origin = NA)
   }
 
-  if (island_scenario == 9) {
-
+  if (island_scenario == 2) {
+    # Single lineage endemic (early col)
     island_spec <- data.frame(
       spec_id = 1,
       main_anc_id = 1,
-      col_t = 0.5,
+      col_t = 0.2,
       spec_type = "A",
       branch_code = NA,
       branch_t = NA,
       ana_origin = "mainland_extinction")
   }
+
+  if (island_scenario == 3) {
+    # Single lineage non-endemic (late col)
+    island_spec <- data.frame(
+      spec_id = 2,
+      main_anc_id = 2,
+      col_t = 0.7,
+      spec_type = "I",
+      branch_code = NA,
+      branch_t = NA,
+      ana_origin = NA)
+  }
+
+  if (island_scenario == 4) {
+    # Single lineage endemic (early col)
+    island_spec <- data.frame(
+      spec_id = 1,
+      main_anc_id = 1,
+      col_t = 0.2,
+      spec_type = "A",
+      branch_code = NA,
+      branch_t = NA,
+      ana_origin = "mainland_extinction")
+  }
+
+  if (island_scenario == 5) {
+    # Single lineage endemic (early col)
+    island_spec <- data.frame(
+      spec_id = 1,
+      main_anc_id = 1,
+      col_t = 0.2,
+      spec_type = "A",
+      branch_code = NA,
+      branch_t = NA,
+      ana_origin = "mainland_extinction")
+  }
+
+  if (island_scenario == 6) {
+    # Single lineage endemic (early col)
+    island_spec <- data.frame(
+      spec_id = 1,
+      main_anc_id = 1,
+      col_t = 0.2,
+      spec_type = "A",
+      branch_code = NA,
+      branch_t = NA,
+      ana_origin = "mainland_extinction")
+  }
+
+  if (island_scenario == 7) {
+    # Single lineage non-endemic (late col)
+    island_spec <- data.frame(
+      spec_id = 3,
+      main_anc_id = 3,
+      col_t = 0.7,
+      spec_type = "I",
+      branch_code = NA,
+      branch_t = NA,
+      ana_origin = NA)
+  }
+
+  if (island_scenario == 8) {
+    # Single lineage endemic (late col)
+    island_spec <- data.frame(
+      spec_id = 3,
+      main_anc_id = 3,
+      col_t = 0.7,
+      spec_type = "A",
+      branch_code = NA,
+      branch_t = NA,
+      ana_origin = "mainland_extinction")
+  }
+
+  if (island_scenario == 9) {
+    # Single lineage endemic (late col)
+    island_spec <- data.frame(
+      spec_id = 3,
+      main_anc_id = 3,
+      col_t = 0.7,
+      spec_type = "A",
+      branch_code = NA,
+      branch_t = NA,
+      ana_origin = "mainland_extinction")
+  }
+
+  if (island_scenario == 10) {
+    # Single lineage endemic (early col)
+    island_spec <- data.frame(
+      spec_id = 2,
+      main_anc_id = 1,
+      col_t = 0.2,
+      spec_type = "A",
+      branch_code = NA,
+      branch_t = NA,
+      ana_origin = "immig_parent")
+  }
+
+  if (island_scenario == 11) {
+    # Single lineage endemic (early col)
+    island_spec <- data.frame(
+      spec_id = 2,
+      main_anc_id = 1,
+      col_t = 0.2,
+      spec_type = "A",
+      branch_code = NA,
+      branch_t = NA,
+      ana_origin = "immig_parent")
+  }
+
+  if (island_scenario == 12) {
+    # Single lineage endemic (late col)
+    island_spec <- data.frame(
+      spec_id = 4,
+      main_anc_id = 2,
+      col_t = 0.7,
+      spec_type = "A",
+      branch_code = NA,
+      branch_t = NA,
+      ana_origin = "immig_parent")
+  }
+
+  if (island_scenario == 13) {
+    # Single lineage endemic (early col)
+    island_spec <- data.frame(
+      spec_id = 2,
+      main_anc_id = 1,
+      col_t = 0.2,
+      spec_type = "A",
+      branch_code = NA,
+      branch_t = NA,
+      ana_origin = "immig_parent")
+  }
+
+  if (island_scenario == 14) {
+    # Single lineage endemic (early col)
+    island_spec <- data.frame(
+      spec_id = 2,
+      main_anc_id = 1,
+      col_t = 0.2,
+      spec_type = "A",
+      branch_code = NA,
+      branch_t = NA,
+      ana_origin = "immig_parent")
+  }
+
+  if (island_scenario == 15) {
+    # Single lineage endemic (early col)
+    island_spec <- data.frame(
+      spec_id = 2,
+      main_anc_id = 1,
+      col_t = 0.2,
+      spec_type = "A",
+      branch_code = NA,
+      branch_t = NA,
+      ana_origin = "immig_parent")
+  }
+
+  if (island_scenario == 16) {
+    # Single lineage endemic (late col)
+    island_spec <- data.frame(
+      spec_id = 4,
+      main_anc_id = 3,
+      col_t = 0.7,
+      spec_type = "A",
+      branch_code = NA,
+      branch_t = NA,
+      ana_origin = "immig_parent")
+  }
+
+  if (island_scenario == 17) {
+    # Single lineage endemic (late col)
+    island_spec <- data.frame(
+      spec_id = 4,
+      main_anc_id = 3,
+      col_t = 0.7,
+      spec_type = "A",
+      branch_code = NA,
+      branch_t = NA,
+      ana_origin = "immig_parent")
+  }
+
+  if (island_scenario == 18) {
+    # Single lineage endemic (late col)
+    island_spec <- data.frame(
+      spec_id = 4,
+      main_anc_id = 3,
+      col_t = 0.7,
+      spec_type = "A",
+      branch_code = NA,
+      branch_t = NA,
+      ana_origin = "immig_parent") #should this be "immig_parent" or "mainland_extinction"?
+  }
+
+  if (island_scenario == 19) {
+    # 2 species clade (early col)
+    island_spec <- data.frame(
+      spec_id = c(2, 3),
+      main_anc_id = c(1, 1),
+      col_t = c(0.2, 0.2),
+      spec_type = c("C", "C"),
+      branch_code = c("A", "B"),
+      branch_t = c(0.2, 0.5),
+      ana_origin = c(NA, NA))
+  }
+
+  if (island_scenario == 20) {
+    # 2 species clade (early col)
+    island_spec <- data.frame(
+      spec_id = c(4, 5),
+      main_anc_id = c(1, 1),
+      col_t = c(0.2, 0.2),
+      spec_type = c("C", "C"),
+      branch_code = c("A", "B"),
+      branch_t = c(0.2, 0.55),
+      ana_origin = c(NA, NA))
+  }
+
+  if (island_scenario == 21) {
+    # 2 species clade (early col)
+    island_spec <- data.frame(
+      spec_id = c(2, 3),
+      main_anc_id = c(1, 1),
+      col_t = c(0.2, 0.2),
+      spec_type = c("C", "C"),
+      branch_code = c("A", "B"),
+      branch_t = c(0.2, 0.55),
+      ana_origin = c(NA, NA))
+  }
+
+  if (island_scenario == 22) {
+    # 2 species clade (late col)
+    island_spec <- data.frame(
+      spec_id = c(4, 5),
+      main_anc_id = c(3, 3),
+      col_t = c(0.7, 0.7),
+      spec_type = c("C", "C"),
+      branch_code = c("A", "B"),
+      branch_t = c(0.7, 0.85),
+      ana_origin = c(NA, NA))
+  }
+
+  if (island_scenario == 23) {
+    # 2 species clade (early col)
+    island_spec <- data.frame(
+      spec_id = c(4, 5),
+      main_anc_id = c(1, 1),
+      col_t = c(0.2, 0.2),
+      spec_type = c("C", "C"),
+      branch_code = c("A", "B"),
+      branch_t = c(0.2, 0.55),
+      ana_origin = c(NA, NA))
+  }
+
+  if (island_scenario == 24) {
+    # 2 species clade (early col)
+    island_spec <- data.frame(
+      spec_id = c(4, 5),
+      main_anc_id = c(1, 1),
+      col_t = c(0.2, 0.2),
+      spec_type = c("C", "C"),
+      branch_code = c("A", "B"),
+      branch_t = c(0.2, 0.55),
+      ana_origin = c(NA, NA))
+  }
+
+  if (island_scenario == 25) {
+    # 2 species clade (late col)
+    island_spec <- data.frame(
+      spec_id = c(4, 5),
+      main_anc_id = c(3, 3),
+      col_t = c(0.7, 0.7),
+      spec_type = c("C", "C"),
+      branch_code = c("A", "B"),
+      branch_t = c(0.7, 0.85),
+      ana_origin = c(NA, NA))
+  }
+
+  if (island_scenario == 26) {
+    # 2 species clade (late col)
+    island_spec <- data.frame(
+      spec_id = c(4, 5),
+      main_anc_id = c(3, 3),
+      col_t = c(0.7, 0.7),
+      spec_type = c("C", "C"),
+      branch_code = c("A", "B"),
+      branch_t = c(0.7, 0.85),
+      ana_origin = c(NA, NA))
+  }
+
+  if (island_scenario == 27) {
+    # 2 species clade (late col)
+    island_spec <- data.frame(
+      spec_id = c(4, 5),
+      main_anc_id = c(3, 3),
+      col_t = c(0.7, 0.7),
+      spec_type = c("C", "C"),
+      branch_code = c("A", "B"),
+      branch_t = c(0.7, 0.85),
+      ana_origin = c(NA, NA))
+  }
+
+  if (island_scenario == 28) {
+    # 2 species clade (early col) & and non-endemic recol
+    island_spec <- data.frame(
+      spec_id = c(2, 3, 1),
+      main_anc_id = c(1, 1, 1),
+      col_t = c(0.2, 0.2, 0.65),
+      spec_type = c("C", "C", "I"),
+      branch_code = c("A", "B", NA),
+      branch_t = c(0.2, 0.55, NaN),
+      ana_origin = c(NA, NA, NA))
+  }
+
+  if (island_scenario == 29) {
+    # 2 species clade (early col) & and endemic recol
+    island_spec <- data.frame(
+      spec_id = c(2, 3, 1),
+      main_anc_id = c(1, 1, 1),
+      col_t = c(0.2, 0.2, 0.65),
+      spec_type = c("C", "C", "A"),
+      branch_code = c("A", "B", NA),
+      branch_t = c(0.2, 0.55, NaN),
+      ana_origin = c(NA, NA, "mainland_extinction"))
+  }
+
+  if (island_scenario == 30) {
+    # 2 species clade (early col) & and endemic recol
+    island_spec <- data.frame(
+      spec_id = c(2, 3, 1),
+      main_anc_id = c(1, 1, 1),
+      col_t = c(0.2, 0.2, 0.65),
+      spec_type = c("C", "C", "A"),
+      branch_code = c("A", "B", NA),
+      branch_t = c(0.2, 0.55, NaN),
+      ana_origin = c(NA, NA, "mainland_extinction"))
+  }
+
+  if (island_scenario == 31) {
+    # 2 species clade (early col) & and endemic recol
+    island_spec <- data.frame(
+      spec_id = c(2, 3, 1),
+      main_anc_id = c(1, 1, 1),
+      col_t = c(0.2, 0.2, 0.65),
+      spec_type = c("C", "C", "A"),
+      branch_code = c("A", "B", NA),
+      branch_t = c(0.2, 0.55, NaN),
+      ana_origin = c(NA, NA, "mainland_extinction"))
+  }
+
+  if (island_scenario == 32) {
+    # 2 species clade (early col) & and endemic recol
+    island_spec <- data.frame(
+      spec_id = c(2, 3, 1),
+      main_anc_id = c(1, 1, 1),
+      col_t = c(0.2, 0.2, 0.65),
+      spec_type = c("C", "C", "A"),
+      branch_code = c("A", "B", NA),
+      branch_t = c(0.2, 0.55, NaN),
+      ana_origin = c(NA, NA, "mainland_extinction"))
+  }
+
+  if (island_scenario == 33) {
+    # 2 species clade (early col) & and non-endemic recol
+    island_spec <- data.frame(
+      spec_id = c(4, 5, 3),
+      main_anc_id = c(3, 3, 3),
+      col_t = c(0.2, 0.2, 0.65),
+      spec_type = c("C", "C", "I"),
+      branch_code = c("A", "B", NA),
+      branch_t = c(0.2, 0.55, NaN),
+      ana_origin = c(NA, NA, NA))
+  }
+
+  if (island_scenario == 34) {
+    # 2 species clade (early col) & and endemic recol
+    island_spec <- data.frame(
+      spec_id = c(4, 5, 3),
+      main_anc_id = c(3, 3, 3),
+      col_t = c(0.2, 0.2, 0.65),
+      spec_type = c("C", "C", "A"),
+      branch_code = c("A", "B", NA),
+      branch_t = c(0.2, 0.55, NaN),
+      ana_origin = c(NA, NA, "mainland_extinction"))
+  }
+
+  if (island_scenario == 35) {
+    # 2 species clade (early col) & and endemic recol
+    island_spec <- data.frame(
+      spec_id = c(4, 5, 3),
+      main_anc_id = c(3, 3, 3),
+      col_t = c(0.2, 0.2, 0.65),
+      spec_type = c("C", "C", "A"),
+      branch_code = c("A", "B", NA),
+      branch_t = c(0.2, 0.55, NaN),
+      ana_origin = c(NA, NA, "mainland_extinction"))
+  }
+
+  if (island_scenario == 36) {
+    # Single lineage endemic (early col) & single lineage non-endemic (late col)
+    island_spec <- data.frame(
+      spec_id = c(1, 3),
+      main_anc_id = c(1, 3),
+      col_t = c(0.2, 0.65),
+      spec_type = c("A", "I"),
+      branch_code = c(NA, NA),
+      branch_t = c(NaN, NaN),
+      ana_origin = c("mainland_extinction", NA))
+  }
+
+  if (island_scenario == 37) {
+    # Single lineage endemic (early col) & single lineage non-endemic (late col)
+    island_spec <- data.frame(
+      spec_id = c(1, 3),
+      main_anc_id = c(1, 3),
+      col_t = c(0.2, 0.65),
+      spec_type = c("A", "I"),
+      branch_code = c(NA, NA),
+      branch_t = c(NaN, NaN),
+      ana_origin = c("mainland_extinction", NA))
+  }
+
+  if (island_scenario == 38) {
+    # Single lineage endemic (early col) & single lineage endemic (late col)
+    island_spec <- data.frame(
+      spec_id = c(1, 3),
+      main_anc_id = c(1, 3),
+      col_t = c(0.2, 0.65),
+      spec_type = c("A", "A"),
+      branch_code = c(NA, NA),
+      branch_t = c(NaN, NaN),
+      ana_origin = c("mainland_extinction", "mainland_extinction"))
+  }
+
+  if (island_scenario == 39) {
+    # Single lineage endemic (early col) & single lineage endemic (late col)
+    island_spec <- data.frame(
+      spec_id = c(1, 3),
+      main_anc_id = c(1, 3),
+      col_t = c(0.2, 0.65),
+      spec_type = c("A", "A"),
+      branch_code = c(NA, NA),
+      branch_t = c(NaN, NaN),
+      ana_origin = c("mainland_extinction", "mainland_extinction"))
+  }
+
+  if (island_scenario == 40) {
+    # Single lineage endemic (early col) & single lineage non-endemic (late col)
+    island_spec <- data.frame(
+      spec_id = c(2, 4),
+      main_anc_id = c(2, 4),
+      col_t = c(0.2, 0.65),
+      spec_type = c("A", "I"),
+      branch_code = c(NA, NA),
+      branch_t = c(NaN, NaN),
+      ana_origin = c("mainland_extinction", NA))
+  }
+
+  if (island_scenario == 41) {
+    # Single lineage endemic (early col) & single lineage non-endemic (late col)
+    island_spec <- data.frame(
+      spec_id = c(2, 4),
+      main_anc_id = c(2, 4),
+      col_t = c(0.2, 0.65),
+      spec_type = c("A", "I"),
+      branch_code = c(NA, NA),
+      branch_t = c(NaN, NaN),
+      ana_origin = c("mainland_extinction", NA))
+  }
+
+  if (island_scenario == 42) {
+    # Single lineage endemic (early col) & single lineage endemic (late col)
+    island_spec <- data.frame(
+      spec_id = c(2, 4),
+      main_anc_id = c(2, 4),
+      col_t = c(0.2, 0.65),
+      spec_type = c("A", "A"),
+      branch_code = c(NA, NA),
+      branch_t = c(NaN, NaN),
+      ana_origin = c("mainland_extinction", "mainland_extinction"))
+  }
+
+  if (island_scenario == 43) {
+    # Single lineage endemic (early col) & single lineage endemic (late col)
+    island_spec <- data.frame(
+      spec_id = c(2, 4),
+      main_anc_id = c(2, 4),
+      col_t = c(0.2, 0.65),
+      spec_type = c("A", "A"),
+      branch_code = c(NA, NA),
+      branch_t = c(NaN, NaN),
+      ana_origin = c("mainland_extinction", "mainland_extinction"))
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   if (island_scenario == 10) {
 
