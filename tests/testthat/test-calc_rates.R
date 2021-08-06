7f16b353428425016a7dc1829011c554cfadbef6
joshwlambert <j.w.l.lambert@rug.nl>
2021-08-06 15:23:21 +0200

    added tests to calc_rates

--- tests/testthat/test-calc_rates.R
+++ tests/testthat/test-calc_rates.R
@@ -13,3 +13,85 @@ test_that("calc_rates produces correct output", {
   expect_equal(rates$ana_rate, 1.0)
   expect_equal(rates$clado_rate, 0.9)
 })
+
+test_that("calc_rates fails with incorrect input", {
+  expect_error(calc_rates(
+    gam = "nonsense",
+    laa = 1.0,
+    lac = 1.0,
+    mu = 1.0,
+    k = 10.0,
+    num_spec = 1,
+    num_immigrants = 1,
+    mainland_n = 10))
+
+  expect_error(calc_rates(
+    gam = 1.0,
+    laa = "nonsense",
+    lac = 1.0,
+    mu = 1.0,
+    k = 10.0,
+    num_spec = 1,
+    num_immigrants = 1,
+    mainland_n = 10))
+
+  expect_error(calc_rates(
+    gam = 1.0,
+    laa = 1.0,
+    lac = "nonsense",
+    mu = 1.0,
+    k = 10.0,
+    num_spec = 1,
+    num_immigrants = 1,
+    mainland_n = 10))
+
+  expect_error(calc_rates(
+    gam = 1.0,
+    laa = 1.0,
+    lac = 1.0,
+    mu = "nonsense",
+    k = 10.0,
+    num_spec = 1,
+    num_immigrants = 1,
+    mainland_n = 10))
+
+  expect_error(calc_rates(
+    gam = 1.0,
+    laa = 1.0,
+    lac = 1.0,
+    mu = 1.0,
+    k = "nonsense",
+    num_spec = 1,
+    num_immigrants = 1,
+    mainland_n = 10))
+
+  expect_error(calc_rates(
+    gam = 1.0,
+    laa = 1.0,
+    lac = 1.0,
+    mu = 1.0,
+    k = 10.0,
+    num_spec = "nonsense",
+    num_immigrants = 1,
+    mainland_n = 10))
+
+  expect_error(calc_rates(
+    gam = 1.0,
+    laa = 1.0,
+    lac = 1.0,
+    mu = 1.0,
+    k = 10.0,
+    num_spec = 1,
+    num_immigrants = "nonsense",
+    mainland_n = 10))
+
+  expect_error(calc_rates(
+    gam = 1.0,
+    laa = 1.0,
+    lac = 1.0,
+    mu = 1.0,
+    k = 10.0,
+    num_spec = 1,
+    num_immigrants = 1,
+    mainland_n = "nonsense"))
+})
