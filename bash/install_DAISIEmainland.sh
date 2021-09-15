1d3d6caead2b91e3a735f4656a8e3f9e16093163
Joshua Lambert <j.w.l.lambert@rug.nl>
2021-09-15 14:16:22 +0100

    updated install_DAISIEmainland to install from branch

--- bash/install_DAISIEmainland.sh
+++ bash/install_DAISIEmainland.sh
@@ -11,4 +11,4 @@ mkdir -p logs
 mkdir -p results
 mkdir -p plots
 ml R
-Rscript -e "remotes::install_github('joshwlambert/DAISIEmainland')"
+Rscript -e "remotes::install_github('joshwlambert/DAISIEmainland@cluster')"
