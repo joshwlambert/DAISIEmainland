f83541d646bd585ed9c9cd93337ea7e99a82a186
Joshua Lambert <j.w.l.lambert@rug.nl>
2021-09-20 10:06:42 +0100

    added test for saving plot_ctt

--- tests/testthat/test-plot_ctt.R
+++ tests/testthat/test-plot_ctt.R
@@ -1,7 +1,20 @@
 test_that("plot_ctt (no save) runs silent without error", {
-  #skip_if(Sys.getenv("CI") == "", message = "Run only on CI") {
     expect_silent(ctt <- plot_ctt(
       data_folder_path = file.path("testdata"),
       output_file_path = NULL))
-  #}
+})
+
+test_that("plot_ctt (save) runs silent without error", {
+  output_filename <- tempfile(
+    pattern = "",
+    tmpdir = tempdir(),
+    fileext = ".png"
+  )
+  expect_false(file.exists(output_filename))
+
+  expect_silent(plot_ctt(data_folder_path = file.path("testdata"),
+                         output_file_path = output_filename))
+
+  file.remove(output_filename)
+  expect_false(file.exists(output_filename))
 })
