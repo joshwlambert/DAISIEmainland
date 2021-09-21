5d84d52e4496c47976b01f0d5b29627c1d5eb68a
Joshua Lambert <j.w.l.lambert@rug.nl>
2021-09-21 13:06:45 +0100

    updated plot_sim_metrics and documentation

--- R/plot_sim_metrics.R
+++ R/plot_sim_metrics.R
@@ -10,9 +10,9 @@
 plot_sim_metrics <- function(sim_metrics) {
 
   plotting_data <- data.frame(
-    number_spec = sim_metrics$number_spec,
-    number_colonists = sim_metrics$number_colonists,
-    size_largest_clade = sim_metrics$size_largest_clade)
+    num_spec = sim_metrics$num_spec,
+    num_col = sim_metrics$num_colonists,
+    max_clade_size = sim_metrics$max_clade_size)
 
   num_spec <- ggplot2::ggplot(data = plotting_data) +
     ggplot2::geom_histogram(ggplot2::aes(x = number_spec),
@@ -25,7 +25,8 @@ plot_sim_metrics <- function(sim_metrics) {
   num_col <- ggplot2::ggplot(data = plotting_data) +
     ggplot2::geom_histogram(ggplot2::aes(x = number_colonists),
                             fill = "cornsilk3") +
-    ggplot2::geom_vline(ggplot2::aes(xintercept = median(number_colonists))) +
+    ggplot2::geom_vline(ggplot2::aes(
+      xintercept = stats::median(number_colonists))) +
     ggplot2::theme_classic() +
     ggplot2::xlab("Number of Colonisation events") +
     ggplot2::ylab("Frequency")
@@ -39,14 +40,4 @@ plot_sim_metrics <- function(sim_metrics) {
     ggplot2::ylab("Frequency")
 
   cowplot::plot_grid(num_spec, num_col, size_largest_clade, nrow = 1)
-
-  bar_colors <- rep('cornsilk3',5)
-  bar_colors[sumstats[4]] <- 'blue'
-  barplot(height = overall_results$tt[, 2],
-          names.arg = c('1st', '2nd', '3rd', '4th', '5th'),
-          col = bar_colors,
-          border = bar_colors,
-          xlab = 'Rank largest clade',
-          ylab = 'Density',
-          ylim = c(0, ylim4))
 }
