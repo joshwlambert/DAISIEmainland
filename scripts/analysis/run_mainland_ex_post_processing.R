a7cc3b7b4f64a750049f986fa525735b686593fa
Joshua Lambert <j.w.l.lambert@rug.nl>
2021-09-21 12:59:36 +0100

    added run_mainland_ex_post_processing script

---
+++ scripts/analysis/run_mainland_ex_post_processing.R
@@ -0,0 +1,7 @@
+plot_ctt_scatter(data_folder_path = file.path("results"),
+                 output_file_path = file.path("plots"),
+                 parameter = "mainland_ex")
+
+plot_ctt_scatter(data_folder_path = file.path("results"),
+                 output_file_path = file.path("plots"),
+                 parameter = "mainland_sample_prob")
