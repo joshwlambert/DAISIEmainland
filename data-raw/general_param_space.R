00aa65d64a220fb86b8d5b94c41d97bcdc37f5bf
Joshua Lambert <j.w.l.lambert@rug.nl>
2021-09-24 09:53:35 +0100

    updated parameter spaces

--- data-raw/general_param_space.R
+++ data-raw/general_param_space.R
@@ -1,13 +1,13 @@
 ## code to prepare `general_param_space` dataset goes here
 general_param_space <- expand.grid(
   total_time = c(5),
-  m = c(500, 1000),
+  m = c(1000),
   island_clado = c(0.5),
   island_ex = c(0.5),
   island_k = c(10, 100),
-  island_immig = c(0.05),
+  island_immig = c(0.01),
   island_ana = c(0.5),
   mainland_ex = c(0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0),
   mainland_sample_prob = c(0.5, 0.6, 0.7, 0.8, 0.9, 1.0),
-  replicates = c(100))
+  replicates = c(10))
 usethis::use_data(general_param_space, overwrite = TRUE)
--- data-raw/mainland_ex_param_space.R
+++ data-raw/mainland_ex_param_space.R
@@ -1,13 +1,13 @@
 ## code to prepare `mainland_ex_param_space` dataset goes here
 mainland_ex_param_space <- expand.grid(
   total_time = c(5),
-  m = c(500),
+  m = c(1000),
   island_clado = c(0.5),
   island_ex = c(0.5),
   island_k = c(10),
-  island_immig = c(0.05),
+  island_immig = c(0.01),
   island_ana = c(0.5),
-  mainland_ex = seq(0, 5, 0.1),
+  mainland_ex = seq(0, 10, 0.1),
   mainland_sample_prob = c(1.0),
-  replicates = c(100))
+  replicates = c(10))
 usethis::use_data(mainland_ex_param_space, overwrite = TRUE)
