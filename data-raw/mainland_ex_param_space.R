543cc8402fd33e702eba713331b183d8c35c7bf2
Joshua Lambert <j.w.l.lambert@rug.nl>
2021-09-28 11:38:19 +0100

    changed limit of mainland_ex back to 5

--- data-raw/mainland_ex_param_space.R
+++ data-raw/mainland_ex_param_space.R
@@ -7,7 +7,7 @@ mainland_ex_param_space <- expand.grid(
   island_k = c(10),
   island_immig = c(0.01),
   island_ana = c(0.5),
-  mainland_ex = seq(0, 10, 0.1),
+  mainland_ex = seq(0, 5, 0.1),
   mainland_sample_prob = c(1.0),
   replicates = c(100))
 usethis::use_data(mainland_ex_param_space, overwrite = TRUE)
