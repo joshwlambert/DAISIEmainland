b3d067964e96968d37e78519dbe1f9305b16dc86
Joshua Lambert <j.w.l.lambert@rug.nl>
2021-09-28 21:37:02 +0100

    deleted run_mainland_ex_post_processing

--- scripts/post_processing/run_mainland_ex_post_processing.R
+++
@@ -1,9 +0,0 @@
-plot_ctt_scatter(
-  data_folder_path = file.path("results"),
-  output_file_path = file.path("plots", "mainland_ex_ctt_scatter.png"),
-  param_space = "mainland_ex")
-
-plot_ctt_scatter(
-  data_folder_path = file.path("results"),
-  output_file_path = file.path("plots", "mainland_sample_prob_scatter.png"),
-  param_space = "mainland_sample_prob")
