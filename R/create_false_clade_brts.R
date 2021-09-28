e69fcc0dc4a897ce067f5a4144ccbbc1acd6a394
Joshua Lambert <j.w.l.lambert@rug.nl>
2021-09-28 11:36:03 +0100

    fixed bug in create_false_clade_brts

--- R/create_false_clade_brts.R
+++ R/create_false_clade_brts.R
@@ -10,14 +10,14 @@ create_false_clade_brts <- function(total_time,
                                     anc_branch_t_bp,
                                     subset_island) {
   if (all(is.na(subset_island[, "branch_t_bp"]))) {
-    false_brts <- subset_island[, "col_t_bp"]
+    false_brts <- unique(sort(subset_island[, "col_t_bp"], decreasing = TRUE))
     false_brts <- false_brts[-length(false_brts)]
   } else {
-    false_brts <- subset_island[, "col_t_bp"]
-    false_brts <- false_brts[-length(false_brts)]
-    false_brts <- unique(false_brts)
-    false_brts <- sort(c(false_brts, subset_island[, "branch_t_bp"]),
-                       decreasing = TRUE)
+    false_brts <- unique(sort(subset_island[, "col_t_bp"], decreasing = TRUE))
+    second_col <- false_brts[length(false_brts)]
+    false_brts <- unique(sort(c(false_brts, subset_island[, "branch_t_bp"]),
+                       decreasing = TRUE))
+    false_brts <- false_brts[-which(false_brts == second_col)]
   }
   false_clade_brts <- c(total_time, anc_branch_t_bp, false_brts)
   return(false_clade_brts)
