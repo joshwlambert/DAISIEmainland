3e6b6454790bf70e81ee2aa09fd8f60718714c32
Joshua Lambert <j.w.l.lambert@rug.nl>
2021-10-07 10:14:46 +0100

    updated parameter spaces

--- data-raw/mainland_sample_prob_param_space.R
+++ data-raw/mainland_sample_prob_param_space.R
@@ -3,7 +3,7 @@ mainland_sample_prob_param_space <- expand.grid(
   total_time = c(5),
   m = c(1000),
   island_clado = c(0.5),
-  island_ex = c(0.5),
+  island_ex = c(0.25),
   island_k = c(10),
   island_immig = c(0.01),
   island_ana = c(0.5),
