0ba2d28a1823a824edfe474a7af704edb5e693da
Joshua Lambert <j.w.l.lambert@rug.nl>
2021-10-12 18:40:48 +0100

    updated tests

--- tests/testthat/test-plot_max_age.R
+++ tests/testthat/test-plot_max_age.R
@@ -1,7 +1,8 @@
 test_that("plot_max_age (no save) runs silent without error", {
   expect_silent(max_age <- plot_max_age(
     data_folder_path = file.path("testdata"),
-    output_file_path = NULL))
+    output_file_path = NULL,
+    parameter = "both"))
 })
 
 test_that("plot_max_age (save) runs silent without error", {
@@ -12,8 +13,10 @@ test_that("plot_max_age (save) runs silent without error", {
   )
   expect_false(file.exists(output_filename))
 
-  expect_silent(plot_max_age(data_folder_path = file.path("testdata"),
-                             output_file_path = output_filename))
+  expect_silent(plot_max_age(
+    data_folder_path = file.path("testdata"),
+    output_file_path = output_filename,
+    parameter = "both"))
 
   file.remove(output_filename)
   expect_false(file.exists(output_filename))
--- tests/testthat/test-plot_param_estimates.R
+++ tests/testthat/test-plot_param_estimates.R
@@ -1,40 +1,9 @@
 test_that("plot_param_estimates (no save) runs silent without error", {
-  #warning from x-axis not plotting all points
-  expect_warning(param_estimates <- plot_param_estimates(
-    param_set = 1,
-    xlim = TRUE,
-    data_folder_path = file.path("testdata"),
-    output_file_path = NULL,
-    param_space = "general"))
-})
-
-test_that("plot_param_estimates (no save) runs silent without error", {
   expect_silent(param_estimates <- plot_param_estimates(
     param_set = 1,
-    xlim = FALSE,
     data_folder_path = file.path("testdata"),
     output_file_path = NULL,
     param_space = "general"))
-})
-
-test_that("plot_param_estimates (save) runs silent without error", {
-  #warning from x-axis not plotting all points
-  output_filename <- tempfile(
-    pattern = "",
-    tmpdir = tempdir(),
-    fileext = ".png"
-  )
-  expect_false(file.exists(output_filename))
-
-  expect_warning(plot_param_estimates(
-    param_set = 1,
-    xlim = TRUE,
-    data_folder_path = file.path("testdata"),
-    output_file_path = output_filename,
-    param_space = "general"))
-
-  file.remove(output_filename)
-  expect_false(file.exists(output_filename))
 })
 
 test_that("plot_param_estimates (save) runs silent without error", {
@@ -47,7 +16,6 @@ test_that("plot_param_estimates (save) runs silent without error", {
 
   expect_silent(plot_param_estimates(
     param_set = 1,
-    xlim = FALSE,
     data_folder_path = file.path("testdata"),
     output_file_path = output_filename,
     param_space = "general"))
