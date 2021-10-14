8510b1f34521e510c18c601db414647b87480804
Joshua Lambert <j.w.l.lambert@rug.nl>
2021-10-14 18:29:11 +0200

    increased number of simulation replicates

--- data-raw/mainland_ex_param_space.R
+++ data-raw/mainland_ex_param_space.R
@@ -9,5 +9,5 @@ mainland_ex_param_space <- expand.grid(
   island_ana = c(0.5),
   mainland_ex = seq(0, 5, 0.1),
   mainland_sample_prob = c(1.0),
-  replicates = c(100))
+  replicates = c(500))
 usethis::use_data(mainland_ex_param_space, overwrite = TRUE)
