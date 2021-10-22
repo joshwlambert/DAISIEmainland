0370b3e9bfae0dcf8d9845275c9f21729c71b813
joshwlambert <j.w.l.lambert@rug.nl>
2021-10-22 15:14:29 +0200

    updated plot_param_estimates tests

--- tests/testthat/test-plot_param_estimates.R
+++ tests/testthat/test-plot_param_estimates.R
@@ -2,7 +2,8 @@ test_that("plot_param_estimates (no save) runs silent without error", {
   expect_silent(param_estimates <- plot_param_estimates(
     param_set = 1,
     data_folder_path = file.path("testdata"),
-    output_file_path = NULL))
+    output_file_path = NULL,
+    parameter = "mainland_ex"))
 })
 
 test_that("plot_param_estimates (save) runs silent without error", {
@@ -16,7 +17,8 @@ test_that("plot_param_estimates (save) runs silent without error", {
   expect_silent(plot_param_estimates(
     param_set = 1,
     data_folder_path = file.path("testdata"),
-    output_file_path = output_filename))
+    output_file_path = output_filename,
+    parameter = "mainland_ex"))
 
   file.remove(output_filename)
   expect_false(file.exists(output_filename))
