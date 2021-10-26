d86c457e6a4011fbe851557baf707d89c59b23f7
joshwlambert <j.w.l.lambert@rug.nl>
2021-10-26 10:50:58 +0200

    updated parameter space and bash scripts

--- bash/run_analysis.sh
+++ bash/run_analysis.sh
@@ -6,7 +6,7 @@
 #SBATCH --job-name=analysis
 #SBATCH --output=/data/p287218/DAISIEmainland/logs/analysis%a.log
 #SBATCH --array=1-46
-#SBATCH --mem=3GB
+#SBATCH --mem=5GB
 
 ml R
 Rscript /data/p287218/DAISIEmainland/scripts/run_analysis.R ${SLURM_ARRAY_TASK_ID}
