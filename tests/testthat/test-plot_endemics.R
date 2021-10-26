d5e3779f136f002534f87c52a0aea122e784e73f
joshwlambert <j.w.l.lambert@rug.nl>
2021-10-26 12:07:48 +0200

    updated plot_endemics and plot_max_age tests

--- tests/testthat/test-plot_endemics.R
+++ tests/testthat/test-plot_endemics.R
@@ -1,11 +1,25 @@
-test_that("plot_endemics (no save) runs silent without error", {
+test_that("plot_endemics mainland_ex (no save) runs silent without error", {
   expect_silent(endemics <- plot_endemics(
     data_folder_path = file.path("testdata"),
     output_file_path = NULL,
     parameter = "mainland_ex"))
 })
 
-test_that("plot_endemics (save) runs silent without error", {
+test_that("plot_endemics unsampled (no save) runs silent without error", {
+  expect_silent(endemics <- plot_endemics(
+    data_folder_path = file.path("testdata"),
+    output_file_path = NULL,
+    parameter = "unsampled"))
+})
+
+test_that("plot_endemics undiscovered (no save) runs silent without error", {
+  expect_silent(endemics <- plot_endemics(
+    data_folder_path = file.path("testdata"),
+    output_file_path = NULL,
+    parameter = "undiscovered"))
+})
+
+test_that("plot_endemics mainland_ex (save) runs silent without error", {
   output_filename <- tempfile(
     pattern = "",
     tmpdir = tempdir(),
@@ -17,6 +31,40 @@ test_that("plot_endemics (save) runs silent without error", {
     data_folder_path = file.path("testdata"),
     output_file_path = output_filename,
     parameter = "mainland_ex"))
+
+  file.remove(output_filename)
+  expect_false(file.exists(output_filename))
+})
+
+test_that("plot_endemics unsampled (save) runs silent without error", {
+  output_filename <- tempfile(
+    pattern = "",
+    tmpdir = tempdir(),
+    fileext = ".png"
+  )
+  expect_false(file.exists(output_filename))
+
+  expect_silent(plot_endemics(
+    data_folder_path = file.path("testdata"),
+    output_file_path = output_filename,
+    parameter = "unsampled"))
+
+  file.remove(output_filename)
+  expect_false(file.exists(output_filename))
+})
+
+test_that("plot_endemics undiscovered (save) runs silent without error", {
+  output_filename <- tempfile(
+    pattern = "",
+    tmpdir = tempdir(),
+    fileext = ".png"
+  )
+  expect_false(file.exists(output_filename))
+
+  expect_silent(plot_endemics(
+    data_folder_path = file.path("testdata"),
+    output_file_path = output_filename,
+    parameter = "undiscovered"))
 
   file.remove(output_filename)
   expect_false(file.exists(output_filename))
--- tests/testthat/test-plot_max_age.R
+++ tests/testthat/test-plot_max_age.R
@@ -1,11 +1,25 @@
-test_that("plot_max_age (no save) runs silent without error", {
+test_that("plot_max_age mainland_ex (no save) runs silent without error", {
   expect_silent(max_age <- plot_max_age(
     data_folder_path = file.path("testdata"),
     output_file_path = NULL,
     parameter = "mainland_ex"))
 })
 
-test_that("plot_max_age (save) runs silent without error", {
+test_that("plot_max_age unsampled (no save) runs silent without error", {
+  expect_silent(max_age <- plot_max_age(
+    data_folder_path = file.path("testdata"),
+    output_file_path = NULL,
+    parameter = "unsampled"))
+})
+
+test_that("plot_max_age undiscovered (no save) runs silent without error", {
+  expect_silent(max_age <- plot_max_age(
+    data_folder_path = file.path("testdata"),
+    output_file_path = NULL,
+    parameter = "undiscovered"))
+})
+
+test_that("plot_max_age mainland_ex (save) runs silent without error", {
   output_filename <- tempfile(
     pattern = "",
     tmpdir = tempdir(),
@@ -17,6 +31,40 @@ test_that("plot_max_age (save) runs silent without error", {
     data_folder_path = file.path("testdata"),
     output_file_path = output_filename,
     parameter = "mainland_ex"))
+
+  file.remove(output_filename)
+  expect_false(file.exists(output_filename))
+})
+
+test_that("plot_max_age unsampled (save) runs silent without error", {
+  output_filename <- tempfile(
+    pattern = "",
+    tmpdir = tempdir(),
+    fileext = ".png"
+  )
+  expect_false(file.exists(output_filename))
+
+  expect_silent(plot_max_age(
+    data_folder_path = file.path("testdata"),
+    output_file_path = output_filename,
+    parameter = "unsampled"))
+
+  file.remove(output_filename)
+  expect_false(file.exists(output_filename))
+})
+
+test_that("plot_max_age undiscovered (save) runs silent without error", {
+  output_filename <- tempfile(
+    pattern = "",
+    tmpdir = tempdir(),
+    fileext = ".png"
+  )
+  expect_false(file.exists(output_filename))
+
+  expect_silent(plot_max_age(
+    data_folder_path = file.path("testdata"),
+    output_file_path = output_filename,
+    parameter = "undiscovered"))
 
   file.remove(output_filename)
   expect_false(file.exists(output_filename))
