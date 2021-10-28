9e63a4ec3d88436640c656fe7e2133acce844b35
joshwlambert <j.w.l.lambert@rug.nl>
2021-10-28 11:13:35 +0200

    updated install bash script

--- bash/install_DAISIEmainland.sh
+++ bash/install_DAISIEmainland.sh
@@ -10,4 +10,4 @@
 mkdir -p logs
 mkdir -p results
 ml R
-Rscript -e "remotes::install_github('joshwlambert/DAISIEmainland@more_tests')"
+Rscript -e "remotes::install_github('joshwlambert/DAISIEmainland@temp_param_space')"
