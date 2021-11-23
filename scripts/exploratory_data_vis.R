512207fac7ad0809bb577c4fc6e2007c020e874f
Joshua Lambert <j.w.l.lambert@rug.nl>
2021-11-23 17:12:42 +0100

    changed scripts names to exploratory_data_vis and final_data_vis and moved calc_sim_metrics to summary_stats script

---
+++ scripts/exploratory_data_vis.R
@@ -0,0 +1,97 @@
+# Produce a range of plots for data visualisation
+
+DAISIEmainland::plot_sim_metrics(
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("plots", "sim_metrics.png"))
+
+DAISIEmainland::plot_ctt_boxplot(
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("plots", "mainland_ex_ctt_scatter.png"),
+  parameter = "mainland_ex")
+
+DAISIEmainland::plot_ctt_boxplot(
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("plots", "unsampled_ctt_scatter.png"),
+  parameter = "unsampled")
+
+DAISIEmainland::plot_ctt_boxplot(
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("plots", "undiscovered_ctt_scatter.png"),
+  parameter = "undiscovered")
+
+data("param_space")
+for (i in seq_along(list.files(file.path("results")))) {
+
+  if (param_space$mainland_sample_type[i] == "complete") {
+    parameter <- "mainland_ex"
+  } else if (param_space$mainland_sample_type[i] == "unsampled") {
+    parameter <- "unsampled"
+  } else if (param_space$mainland_sample_type[i] == "undiscovered") {
+    parameter <- "undiscovered"
+  }
+
+  DAISIEmainland::plot_param_estimates(
+    param_set = i,
+    data_folder_path = file.path("results"),
+    output_file_path = file.path("plots",
+                                 paste0("param_estimates_", i, ".png")),
+    parameter = parameter,
+    num_breaks = 5,
+    signif = 3
+  )
+}
+
+max_age <- DAISIEmainland::plot_max_age(
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("plots", "max_age_mainland_ex.png"),
+  parameter = "mainland_ex")
+
+endemics <- DAISIEmainland::plot_endemics(
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("plots", "endemics_mainland_ex.png"),
+  parameter = "mainland_ex")
+
+max_age <- DAISIEmainland::plot_max_age(
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("plots", "max_age_unsampled.png"),
+  parameter = "unsampled")
+
+endemics <- DAISIEmainland::plot_endemics(
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("plots", "endemics_unsampled.png"),
+  parameter = "unsampled")
+
+max_age <- DAISIEmainland::plot_max_age(
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("plots", "max_age_undiscovered.png"),
+  parameter = "undiscovered")
+
+endemics <- DAISIEmainland::plot_endemics(
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("plots", "endemics_undiscovered.png"),
+  parameter = "undiscovered")
+
+DAISIEmainland::plot_k_estimates(
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("plots", "k_estimates_mainland_ex.png"),
+  parameter = "mainland_ex",
+  num_breaks = 4,
+  signif = 2)
+
+DAISIEmainland::plot_k_estimates(
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("plots", "k_estimates_unsampled.png"),
+  parameter = "unsampled",
+  num_breaks = 4,
+  signif = 2)
+
+DAISIEmainland::plot_k_estimates(
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("plots", "k_estimates_undiscovered.png"),
+  parameter = "undiscovered",
+  num_breaks = 4,
+  signif = 2)
+
+DAISIEmainland::plot_inf_k(
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("plots", "inf_k.png"))
--- scripts/final_data_vis.R
+++ scripts/final_data_vis.R
@@ -1,27 +1,8 @@
-# Produce summary statistic and a range of plots for data visualisation
-
-DAISIEmainland::calc_sim_metrics(
-  data_folder_path = file.path("results"),
-  output_file_path = NULL)
+# Final data visualisatio to produce the plots for the paper
 
 DAISIEmainland::plot_sim_metrics(
   data_folder_path = file.path("results"),
   output_file_path = file.path("plots", "sim_metrics.png"))
-
-DAISIEmainland::plot_ctt_boxplot(
-  data_folder_path = file.path("results"),
-  output_file_path = file.path("plots", "mainland_ex_ctt_scatter.png"),
-  parameter = "mainland_ex")
-
-DAISIEmainland::plot_ctt_boxplot(
-  data_folder_path = file.path("results"),
-  output_file_path = file.path("plots", "unsampled_ctt_scatter.png"),
-  parameter = "unsampled")
-
-DAISIEmainland::plot_ctt_boxplot(
-  data_folder_path = file.path("results"),
-  output_file_path = file.path("plots", "undiscovered_ctt_scatter.png"),
-  parameter = "undiscovered")
 
 ctt_mainland_ex <- DAISIEmainland::plot_ctt_boxplot(
   data_folder_path = file.path("results"),
@@ -44,33 +25,11 @@ ggplot2::ggsave(
   plot = ctt,
   filename = file.path("plots", "ctt.png"),
   device = "png",
-  width = 168,
-  height = 100,
+  width = 160,
+  height = 80,
   units = "mm",
   dpi = 600
 )
-
-data("param_space")
-for (i in seq_along(list.files(file.path("results")))) {
-
-  if (param_space$mainland_sample_type[i] == "complete") {
-    parameter <- "mainland_ex"
-  } else if (param_space$mainland_sample_type[i] == "unsampled") {
-    parameter <- "unsampled"
-  } else if (param_space$mainland_sample_type[i] == "undiscovered") {
-    parameter <- "undiscovered"
-  }
-
-  DAISIEmainland::plot_param_estimates(
-    param_set = i,
-    data_folder_path = file.path("results"),
-    output_file_path = file.path("plots",
-                                 paste0("param_estimates_", i, ".png")),
-    parameter = parameter,
-    num_breaks = 4,
-    signif = 2
-  )
-}
 
 max_age <- DAISIEmainland::plot_max_age(
   data_folder_path = file.path("results"),
@@ -88,8 +47,8 @@ ggplot2::ggsave(
   plot = max_age_and_endemics,
   filename = file.path("plots", "max_age_and_endemics_mainland_ex.png"),
   device = "png",
-  width = 168,
-  height = 100,
+  width = 180,
+  height = 80,
   units = "mm",
   dpi = 600
 )
@@ -110,8 +69,8 @@ ggplot2::ggsave(
   plot = max_age_and_endemics,
   filename = file.path("plots", "max_age_and_endemics_unsampled.png"),
   device = "png",
-  width = 168,
-  height = 100,
+  width = 180,
+  height = 80,
   units = "mm",
   dpi = 600
 )
@@ -132,10 +91,73 @@ ggplot2::ggsave(
   plot = max_age_and_endemics,
   filename = file.path("plots", "max_age_and_endemics_undiscovered.png"),
   device = "png",
-  width = 168,
-  height = 100,
+  width = 180,
+  height = 80,
   units = "mm",
   dpi = 600
+)
+
+DAISIEmainland::plot_param_estimates(
+  param_set = 1,
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("plots", "param_estimates_1.png"),
+parameter = "mainland_ex",
+num_breaks = 5,
+signif = 3
+)
+
+DAISIEmainland::plot_param_estimates(
+  param_set = 3,
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("plots", "param_estimates_3.png"),
+parameter = "mainland_ex",
+num_breaks = 5,
+signif = 2
+)
+
+DAISIEmainland::plot_param_estimates(
+  param_set = 21,
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("plots", "param_estimates_21.png"),
+parameter = "mainland_ex",
+num_breaks = 5,
+signif = 2
+)
+
+DAISIEmainland::plot_param_estimates(
+  param_set = 35,
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("plots", "param_estimates_35.png"),
+parameter = "undiscovered",
+num_breaks = 5,
+signif = 2
+)
+
+DAISIEmainland::plot_param_estimates(
+  param_set = 43,
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("plots", "param_estimates_43.png"),
+parameter = "undiscovered",
+num_breaks = 5,
+signif = 2
+)
+
+DAISIEmainland::plot_param_estimates(
+  param_set = 23,
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("plots", "param_estimates_23.png"),
+parameter = "unsampled",
+num_breaks = 5,
+signif = 2
+)
+
+DAISIEmainland::plot_param_estimates(
+  param_set = 31,
+  data_folder_path = file.path("results"),
+  output_file_path = file.path("plots", "param_estimates_31.png"),
+parameter = "unsampled",
+num_breaks = 5,
+signif = 2
 )
 
 DAISIEmainland::plot_k_estimates(
