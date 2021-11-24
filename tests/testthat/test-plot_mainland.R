3e7261aae7a3933a53a17b7566ee3ae31f34abf6
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-24 21:34:16 +0100

    Import dplyr

--- tests/testthat/test-plot_mainland.R
+++ tests/testthat/test-plot_mainland.R
@@ -12,3 +12,18 @@ test_that("minimal example use", {
 
   plot_mainland(mainland)
 })
+
+test_that("interesting example", {
+  set.seed(
+    4,
+    kind = "Mersenne-Twister",
+    normal.kind = "Inversion",
+    sample.kind = "Rejection"
+  )
+  mainland <- DAISIEmainland:::sim_mainland(
+    total_time = 1,
+    m = 4,
+    mainland_ex = 1)
+
+  plot_mainland(mainland)
+})
