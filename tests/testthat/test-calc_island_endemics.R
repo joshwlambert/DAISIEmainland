9da2733ed15b8ddb3ce1f79112e3fee8107b413d
Joshua Lambert <j.w.l.lambert@rug.nl>
2021-12-04 13:43:23 +0100

    added calc_island_endemics and empty tests

---
+++ tests/testthat/test-calc_island_endemics.R
@@ -0,0 +1,27 @@
+test_that("calc_island_endemics produces is correct for all endemics", {
+
+})
+
+test_that("calc_island_endemics produces is correct for all non-endemics", {
+
+})
+
+test_that("calc_island_endemics produces is correct for endemics and non-endemics", {
+
+})
+
+test_that("calc_island_endemics produces is correct for empty island", {
+
+})
+
+test_that("calc_island_endemics produces is correct for all endemics with recolonisations", {
+
+})
+
+test_that("calc_island_endemics produces is correct for all non-endemics with recolonisations", {
+
+})
+
+test_that("calc_island_endemics produces is correct for endemics and non-endemics with recolonisations", {
+
+})
