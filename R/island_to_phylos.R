847903b3357575ec8525fbfbea624d4d906217dd
Joshua Lambert <j.w.l.lambert@rug.nl>
2021-12-23 19:55:29 +0000

    happy with how island_to_phylos is working (for now)

--- R/island_to_phylos.R
+++ R/island_to_phylos.R
@@ -17,41 +17,43 @@
 #'   replicates = 1)
 #' island_phylos <- island_to_phylos(island = island$ideal_islands)
 island_to_phylos <- function(island) {
-  browser()
   testit::assert(is.list(island))
   if (length(island[[1]]) == 1) {
     stop("Empty island")
   }
   island_age <- island[[1]][[1]]$island_age
   island <- island[[1]][-1]
-  all_times <- lapply(island, "[[", "branching_times")
-  phylos <- lapply(all_times, function(x) {
+  branching_times <- lapply(island, "[[", "branching_times")
+  stacs <- unlist(lapply(island, "[[", "stac"))
+  branching_times_no_recol <- branching_times[which(stacs != 3)]
+  phylos_no_recols <- lapply(branching_times_no_recol, function(x) {
     if (length(x) == 2) {
       create_singleton_phylo(x[2])
     } else if (length(x) > 2) {
-      branching_times <- x[-1]
-      DDD::brts2phylo(branching_times, root = TRUE)
+      DDD::brts2phylo(x[-1], root = TRUE)
     } else {
       stop("no species in island clade")
     }
   })
 
-  stacs <- unlist(lapply(island, "[[", "stac"))
   if (any(stacs == 3)) {
     recol <- island[which(stacs == 3)]
     all_cols <- lapply(recol, "[[", "all_colonisations")
     event_times <- lapply(all_cols,
                           function(x) {lapply(x, "[[", "event_times")})
-    col_times <- lapply(event_times, "[[", 2)
-    branching_times <- lapply(event_times, function(x) {
-      lapply(x, function(y)  {if (length(y) > 2) return(y[-c(1)])})
+    recol_phylos <- lapply(event_times, function(x) {
+      lapply(x,  function(y) {
+        if (length(y) == 2) {
+          create_singleton_phylo(y[2])
+        } else if (length(y) > 2) {
+          DDD::brts2phylo(y[-1], root = TRUE)
+        } else {
+          stop("no species in island clade")
+        }
+      })
     })
-    stac_3_phylos <- lapply(branching_times, function(x) {
-      lapply(x,  function(y) {if (!is.null(x)) DDD::brts2phylo(x, root = TRUE)})
-    })
-    null_phylos <- which(sapply(stac_3_phylos, is.null))
-    stac_3_phylos <- stac_3_phylos[-null_phylos]
-    phylos <- c(phylos, stac_3_phylos)
+    recol_phylos <- unlist(recol_phylos, recursive = FALSE)
+    phylos <- c(phylos, recol_phylos)
   }
   if (length(phylos) == 1) {
     class(phylos) <- "phylo"
