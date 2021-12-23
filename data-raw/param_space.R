3af6731cd33774b926d9da82e1597cbc131d6a0d
Joshua Lambert <j.w.l.lambert@rug.nl>
2021-12-23 13:52:53 +0000

    updated param space

--- data-raw/param_space.R
+++ data-raw/param_space.R
@@ -10,7 +10,7 @@ mainland_ex_param_space <- expand.grid(
   mainland_ex = c(0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0),
   mainland_sample_prob = c(1.0),
   mainland_sample_type = "complete",
-  replicates = c(10),
+  replicates = c(500),
   stringsAsFactors = FALSE)
 seed <- sample(x = 1:100000, size = nrow(mainland_ex_param_space))
 mainland_ex_param_space <- cbind(mainland_ex_param_space, seed)
@@ -26,7 +26,7 @@ unsampled_param_space <- expand.grid(
   mainland_ex = c(0),
   mainland_sample_prob = c(0.5, 0.6, 0.7, 0.8, 0.9, 1.0),
   mainland_sample_type = c("unsampled"),
-  replicates = c(10),
+  replicates = c(500),
   stringsAsFactors = FALSE)
 seed <- sample(x = 1:100000, size = nrow(unsampled_param_space))
 unsampled_param_space <- cbind(unsampled_param_space, seed)
@@ -42,7 +42,7 @@ undiscovered_param_space <- expand.grid(
   mainland_ex = c(0),
   mainland_sample_prob = c(0.5, 0.6, 0.7, 0.8, 0.9, 1.0),
   mainland_sample_type = c("undiscovered"),
-  replicates = c(10),
+  replicates = c(500),
   stringsAsFactors = FALSE)
 undiscovered_param_space <- cbind(undiscovered_param_space, seed)
 
