bdab5e1ff2e26ddcf205303058ec61487e94f908
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-01-10 14:10:55 +0000

    updatded calc_sim_metrics tests

--- tests/testthat/test-calc_sim_metrics.R
+++ tests/testthat/test-calc_sim_metrics.R
@@ -1,21 +1,25 @@
 test_that("calc_sim_metrics runs silent without error", {
+  analysis_results <- read_analysis_results(
+    data_folder_path = file.path("testdata")
+  )
+
   expect_silent(sim_metrics <- calc_sim_metrics(
-    data_folder_path = file.path("testdata"),
+    analysis_results = analysis_results,
     output_file_path = NULL))
 
   expect_length(sim_metrics, 12)
-  expect_equal(sim_metrics$ideal_mean_num_col, 16.9369565217)
-  expect_equal(sim_metrics$ideal_max_num_col, 30)
-  expect_equal(sim_metrics$ideal_min_num_col, 6)
-  expect_equal(sim_metrics$ideal_mean_num_spec, 36.6630434783)
-  expect_equal(sim_metrics$ideal_max_num_spec, 87)
-  expect_equal(sim_metrics$ideal_min_num_spec, 7)
-  expect_equal(sim_metrics$empirical_mean_num_col, 16.7586956522)
-  expect_equal(sim_metrics$empirical_max_num_col, 28)
-  expect_equal(sim_metrics$empirical_min_num_col, 6)
-  expect_equal(sim_metrics$empirical_mean_num_spec, 36.6630434783)
-  expect_equal(sim_metrics$empirical_max_num_spec, 87)
-  expect_equal(sim_metrics$empirical_min_num_spec, 7)
+  expect_equal(sim_metrics$ideal_mean_num_col, 34.2652173913)
+  expect_equal(sim_metrics$ideal_max_num_col, 48)
+  expect_equal(sim_metrics$ideal_min_num_col, 17)
+  expect_equal(sim_metrics$ideal_mean_num_spec, 74.7152173913)
+  expect_equal(sim_metrics$ideal_max_num_spec, 140)
+  expect_equal(sim_metrics$ideal_min_num_spec, 32)
+  expect_equal(sim_metrics$empirical_mean_num_col, 33.9586956522)
+  expect_equal(sim_metrics$empirical_max_num_col, 48)
+  expect_equal(sim_metrics$empirical_min_num_col, 17)
+  expect_equal(sim_metrics$empirical_mean_num_spec, 74.7152173913)
+  expect_equal(sim_metrics$empirical_max_num_spec, 140)
+  expect_equal(sim_metrics$empirical_min_num_spec, 32)
 })
 
 test_that("calc_sim_metrics (save) runs silent without error", {
@@ -26,9 +30,14 @@ test_that("calc_sim_metrics (save) runs silent without error", {
   )
   expect_false(file.exists(output_filename))
 
+  analysis_results <- read_analysis_results(
+    data_folder_path = file.path("testdata")
+  )
+
   expect_silent(sim_metrics <- calc_sim_metrics(
-    data_folder_path = file.path("testdata"),
-    output_file_path = output_filename))
+    analysis_results = analysis_results,
+    output_file_path = output_filename
+  ))
 
   file.remove(output_filename)
   expect_false(file.exists(output_filename))
