4c22d6405cb4465961b535b769417d23f3c39fbe
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-01-12 09:55:37 +0000

    rewrote internals of calc_ctt and calc_max_age_percent

--- R/calc_ctt.R
+++ R/calc_ctt.R
@@ -7,62 +7,69 @@
 #' @author Joshua W. Lambert
 calc_ctt <- function(daisie_mainland_data) {
 
-  delta_ctt_vec <- c()
-
   testit::assert(identical(
     length(daisie_mainland_data$ideal_multi_daisie_data),
     length(daisie_mainland_data$empirical_multi_daisie_data)
   ))
 
+  # extract multi daisie data from daisie mainland data
+  ideal_multi_daisie_data <- daisie_mainland_data$ideal_multi_daisie_data
+  empirical_multi_daisie_data <-
+    daisie_mainland_data$empirical_multi_daisie_data
+  # remove daisie meta data from each daisie_data
+  ideal_multi_daisie_data <- lapply(ideal_multi_daisie_data, function(x) {
+    x[-1]
+  })
+  empirical_multi_daisie_data <- lapply(
+    empirical_multi_daisie_data,
+    function(x) {
+      x[-1]
+    })
+
+  # extract ideal colonisation times
+  ideal_col_times <- lapply(ideal_multi_daisie_data, function(x) {
+    lapply(x, function(y) {
+      if (y$stac %in% c(1, 2, 4, 5, 6)) {
+        y$branching_times[2]
+      } else {
+        lapply(y$all_colonisations, function(z) {
+          z$event_times[2]
+        })
+      }
+    })
+  })
+
+  # extract empirical colonisation times
+  empirical_col_times <- lapply(empirical_multi_daisie_data, function(x) {
+    lapply(x, function(y) {
+      if (y$stac %in% c(1, 2, 4, 5, 6)) {
+        y$branching_times[2]
+      } else {
+        lapply(y$all_colonisations, function(z) {
+          z$event_times[2]
+        })
+      }
+    })
+  })
+
+  # create a vector of colonisation times for each replicate
+  ideal_col_times_list <- lapply(ideal_col_times, unlist)
+  empirical_col_times_list <- lapply(empirical_col_times, unlist)
+
+  delta_ctt_vec <- c()
   for (i in seq_along(daisie_mainland_data$ideal_multi_daisie_data)) {
-
-    ideal_col_times <- c()
-    empirical_col_times <- c()
-
-    for (j in 2:length(daisie_mainland_data$ideal_multi_daisie_data[[i]])) {
-      ideal_stac <- daisie_mainland_data$ideal_multi_daisie_data[[i]][[j]]$stac
-      if (ideal_stac %in% c(1, 2, 4, 5, 6)) {
-        ideal_col_times <- c(
-          ideal_col_times,
-          daisie_mainland_data$ideal_multi_daisie_data[[i]][[j]]$branching_times[2]
-        )
-      } else {
-        for (k in seq_along(daisie_mainland_data$ideal_multi_daisie_data[[i]][[j]]$all_colonisations)) {
-          ideal_col_times <- c(
-            ideal_col_times,
-            daisie_mainland_data$ideal_multi_daisie_data[[i]][[j]]$all_colonisations[[k]]$event_times[[2]]
-          )
-        }
-      }
-    }
-
-    for (j in 2:length(daisie_mainland_data$empirical_multi_daisie_data[[i]])) {
-      empirical_stac <- daisie_mainland_data$empirical_multi_daisie_data[[i]][[j]]$stac
-      if (empirical_stac %in% c(1, 2, 4, 5, 6)) {
-        empirical_col_times <- c(
-          empirical_col_times,
-          daisie_mainland_data$empirical_multi_daisie_data[[i]][[j]]$branching_times[2]
-        )
-      } else {
-        for (k in seq_along(daisie_mainland_data$empirical_multi_daisie_data[[i]][[j]]$all_colonisations)) {
-          empirical_col_times <- c(
-            empirical_col_times,
-            daisie_mainland_data$empirical_multi_daisie_data[[i]][[j]]$all_colonisations[[k]]$event_times[[2]]
-          )
-        }
-      }
-    }
-
     # normalise data
+    ideal_col_times <- ideal_col_times_list[[i]]
     ideal_col_times <- sort(ideal_col_times, decreasing = TRUE)
     ideal_norm_col_times <- 1 - ideal_col_times / max(ideal_col_times)
     ideal_lineages <- seq_along(ideal_norm_col_times)
     ideal_lineages_norm <- ideal_lineages / max(ideal_lineages)
     testit::assert(all(ideal_norm_col_times >= 0 & ideal_norm_col_times <= 1))
     testit::assert(all(ideal_lineages_norm >= 0 & ideal_lineages_norm <= 1))
-    ideal_norm_col_times <- c(ideal_norm_col_times, 1) #check with Richel why this is needed
-    ideal_lineages_norm <- c(ideal_lineages_norm, 1) #check with Richel why this is needed
+    ideal_norm_col_times <- c(ideal_norm_col_times, 1) # nolint check with Richel why this is needed
+    ideal_lineages_norm <- c(ideal_lineages_norm, 1) # nolint check with Richel why this is needed
 
+    empirical_col_times <- empirical_col_times_list[[i]]
     empirical_col_times <- sort(empirical_col_times, decreasing = TRUE)
     empirical_norm_col_times <-
       1 - empirical_col_times / max(empirical_col_times)
@@ -72,8 +79,8 @@ calc_ctt <- function(daisie_mainland_data) {
                          empirical_norm_col_times <= 1))
     testit::assert(all(empirical_lineages_norm >= 0 &
                          empirical_lineages_norm <= 1))
-    empirical_norm_col_times <- c(empirical_norm_col_times, 1) #check with Richel why this is needed
-    empirical_lineages_norm <- c(empirical_lineages_norm, 1) #check with Richel why this is needed
+    empirical_norm_col_times <- c(empirical_norm_col_times, 1) # nolint check with Richel why this is needed
+    empirical_lineages_norm <- c(empirical_lineages_norm, 1) # nolint check with Richel why this is needed
 
     # calculate delta colonisation through time
     delta_ctt <- nLTT::nltt_diff_exact_norm_brts(
@@ -85,5 +92,5 @@ calc_ctt <- function(daisie_mainland_data) {
 
     delta_ctt_vec <- c(delta_ctt_vec, delta_ctt)
   }
-  return(delta_ctt_vec)
+  delta_ctt_vec
 }
--- R/calc_max_age_percent.R
+++ R/calc_max_age_percent.R
@@ -10,66 +10,74 @@ calc_max_age_percent <- function(daisie_mainland_data) {
   time <- daisie_mainland_data$ideal_multi_daisie_data[[1]][[1]]$island_age
   max_age <- time - 1e-5
 
-  ideal_max_age_vec <- c()
-  empirical_max_age_vec <- c()
-
   testit::assert(identical(
     length(daisie_mainland_data$ideal_multi_daisie_data),
     length(daisie_mainland_data$empirical_multi_daisie_data)
   ))
 
+  # extract multi daisie data from daisie mainland data
+  ideal_multi_daisie_data <- daisie_mainland_data$ideal_multi_daisie_data
+  empirical_multi_daisie_data <-
+    daisie_mainland_data$empirical_multi_daisie_data
+  # remove daisie meta data from each daisie_data
+  ideal_multi_daisie_data <- lapply(ideal_multi_daisie_data, function(x) {
+    x[-1]
+  })
+  empirical_multi_daisie_data <- lapply(
+    empirical_multi_daisie_data,
+    function(x) {
+      x[-1]
+    })
+
+  # extract ideal colonisation times
+  ideal_col_times <- lapply(ideal_multi_daisie_data, function(x) {
+    lapply(x, function(y) {
+      if (y$stac %in% c(1, 2, 4, 5, 6)) {
+        y$branching_times[2]
+      } else {
+        lapply(y$all_colonisations, function(z) {
+          z$event_times[2]
+        })
+      }
+    })
+  })
+
+  # extract empirical colonisation times
+  empirical_col_times <- lapply(empirical_multi_daisie_data, function(x) {
+    lapply(x, function(y) {
+      if (y$stac %in% c(1, 2, 4, 5, 6)) {
+        y$branching_times[2]
+      } else {
+        lapply(y$all_colonisations, function(z) {
+          z$event_times[2]
+        })
+      }
+    })
+  })
+
+  # create a vector of colonisation times for each replicate
+  ideal_col_times_list <- lapply(ideal_col_times, unlist)
+  empirical_col_times_list <- lapply(empirical_col_times, unlist)
+
+  ideal_max_age_vec <- c()
+  empirical_max_age_vec <- c()
   for (i in seq_along(daisie_mainland_data$ideal_multi_daisie_data)) {
-    ideal_col_times <- c()
-    empirical_col_times <- c()
-
-    for (j in 2:length(daisie_mainland_data$ideal_multi_daisie_data[[i]])) {
-      ideal_stac <- daisie_mainland_data$ideal_multi_daisie_data[[i]][[j]]$stac
-      if (ideal_stac %in% c(1, 2, 4, 5, 6)) {
-        ideal_col_times <- c(
-          ideal_col_times,
-          daisie_mainland_data$ideal_multi_daisie_data[[i]][[j]]$branching_times[2]
-        )
-      } else {
-        for (k in seq_along(daisie_mainland_data$ideal_multi_daisie_data[[i]][[j]]$all_colonisations)) {
-          ideal_col_times <- c(
-            ideal_col_times,
-            daisie_mainland_data$ideal_multi_daisie_data[[i]][[j]]$all_colonisations[[k]]$event_times[[2]]
-          )
-        }
-      }
-    }
-
-    for (j in 2:length(daisie_mainland_data$empirical_multi_daisie_data[[i]])) {
-      empirical_stac <- daisie_mainland_data$empirical_multi_daisie_data[[i]][[j]]$stac
-      if (empirical_stac %in% c(1, 2, 4, 5, 6)) {
-        empirical_col_times <- c(
-          empirical_col_times,
-          daisie_mainland_data$empirical_multi_daisie_data[[i]][[j]]$branching_times[2]
-        )
-      } else {
-        for (k in seq_along(daisie_mainland_data$empirical_multi_daisie_data[[i]][[j]]$all_colonisations)) {
-          empirical_col_times <- c(
-            empirical_col_times,
-            daisie_mainland_data$ideal_multi_daisie_data[[i]][[j]]$all_colonisations[[k]]$event_times[[2]]
-          )
-        }
-      }
-    }
+    ideal_col_times <- ideal_col_times_list[[i]]
+    empirical_col_times <- empirical_col_times_list[[i]]
     #calc max age percentage
     ideal_max_age <- (length(which(ideal_col_times == max_age)) /
-      length(ideal_col_times)) * 100
+                        length(ideal_col_times)) * 100
     testit::assert(ideal_max_age == 0)
 
     empirical_max_age <- (length(which(empirical_col_times == max_age)) /
-      length(empirical_col_times)) * 100
+                            length(empirical_col_times)) * 100
 
     ideal_max_age_vec <- c(ideal_max_age_vec, ideal_max_age)
     empirical_max_age_vec <- c(empirical_max_age_vec, empirical_max_age)
 
   }
-
   max_age_percent_list <- list(ideal_max_age = ideal_max_age_vec,
                                empirical_max_age = empirical_max_age_vec)
 
-  return(max_age_percent_list)
+  max_age_percent_list
 }
