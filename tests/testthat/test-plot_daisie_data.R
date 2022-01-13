b01cacaa51d3af29c0b847bd3cc2500e359e2f1f
richelbilderbeek <richel@richelbilderbeek.nl>
2022-01-13 14:42:42 +0100

    Add code copied from email

--- tests/testthat/test-plot_daisie_data.R
+++ tests/testthat/test-plot_daisie_data.R
@@ -1,3 +1,27 @@
+test_that("Code copied from email", {
+  skip("Not yet")
+  set.seed(
+    2,
+    kind = "Mersenne-Twister",
+    normal.kind = "Inversion",
+    sample.kind = "Rejection"
+  )
+  daisie_mainland_data <- sim_island_with_mainland(
+    total_time = 1,
+    m = 10,
+    island_pars = c(1, 1, 10, 0.1, 1),
+    mainland_ex = 1,
+    mainland_sample_prob = 1,
+    mainland_sample_type = "undiscovered",
+    replicates = 10,
+    verbose = FALSE
+  )
+
+  plot_daisie_mainland_data(daisie_mainland_data, replicate_index = 1) # Probably calls the next two functions :-)
+  plot_empirical_multi_daisie_data(daisie_mainland_data$empirical_multi_daisie_data, replicate_index = 1)
+  plot_ideal_multi_daisie_data(daisie_mainland_data$ideal_multi_daisie_data, replicate_index = 1)
+})
+
 test_that("Arguments 4 must have names", {
   skip("WIP, #42")
   set.seed(
