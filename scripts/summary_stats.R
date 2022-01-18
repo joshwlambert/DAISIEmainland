7241d17a07cd843aeba3594625400d9c21e66141
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-01-18 21:11:57 +0000

    added joss_data_vis and updated summary_stats scripts

--- scripts/summary_stats.R
+++ scripts/summary_stats.R
@@ -1,5 +1,9 @@
 # Produce summary statistic
 
+analysis_results <- DAISIEmainland::read_analysis_results(
+  data_folder_path = file.path("results")
+)
+
 DAISIEmainland::calc_sim_metrics(
   analysis_results = analysis_results,
   output_file_path = NULL)
