c8b9327d5c9099f806182e8f71ab9bd5a7f1627d
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-04-15 10:17:45 +0200

    add _bookdown.yml and removed file numbering and updated build_book script

--- scripts/build_book.R
+++ scripts/build_book.R
@@ -7,22 +7,7 @@
 # index.Rmd to render the book from vignettes folder
 xfun::in_dir(dir = "inst/book/", expr = bookdown::render_book(
   input = "index.Rmd",
-  output_format = "bookdown::gitbook",
-  output_dir = "docs"))
-
-# create docs folder to host book
-if (isFALSE(dir.exists("docs"))) {
-  dir.create("docs")
-}
-
-# move rendered book from vignettes docs
-file.copy(
-  from = list.files("inst/book/docs", full.names = TRUE),
-  to = "docs",
-  recursive = TRUE,
-  overwrite = TRUE
-)
+  output_format = "bookdown::gitbook"))
 
 # delete the docs and _bookdown_files from vignettes directory
-unlink(x = "inst/book/docs", recursive = TRUE)
 unlink(x = "inst/book/_bookdown_files", recursive = TRUE)
