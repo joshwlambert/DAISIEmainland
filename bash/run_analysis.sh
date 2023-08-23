b1842d76498b8e0ee1db41fb119ed5f23bf391d4
joshwlambert <joshua.lambert@lshtm.ac.uk>
2023-08-23 22:59:53 +0100

    updated bash scripts for habrok

--- bash/run_analysis.sh
+++ bash/run_analysis.sh
@@ -4,9 +4,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=analysis
-#SBATCH --output=/data/p287218/DAISIEmainland/logs/analysis%a.log
+#SBATCH --output=/home3/p287218/DAISIEmainland/logs/analysis%a.log
 #SBATCH --array=1-46
 #SBATCH --mem=5GB
 
 ml R
-Rscript /data/p287218/DAISIEmainland/scripts/run_analysis.R ${SLURM_ARRAY_TASK_ID}
+Rscript /home3/p287218/DAISIEmainland/scripts/run_analysis.R ${SLURM_ARRAY_TASK_ID}
